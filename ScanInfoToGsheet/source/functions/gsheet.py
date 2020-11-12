16e3ba832af7ba121d3a0f4f2f7c2a28dbc16cab
Fumika Isono <fisono@bella.lbl.gov>
2020-11-12 22:21:53 +0000

    fixed bug

--- ScanInfoToGsheet/source/functions/gsheet.py
+++ ScanInfoToGsheet/source/functions/gsheet.py
@@ -2,6 +2,9 @@
 """
 
 import time
+import os
+import pygsheets
+
 from functions.scaninfo import ScanInfo
 
 
@@ -44,7 +47,7 @@ def df2gsheet(sheet_title, df, gdrive_dir):
     """
     Open (Create if not exist) a google sheet in the google drive, then write down a given dataframe
     """
-    dir_path = os.path.dirname(os.path.realpath(__file__))    
+    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))   
     gc = pygsheets.authorize(service_file = dir_path+'\\service_account.json')
 
     #if can't find an exisiting google sheet, create it
