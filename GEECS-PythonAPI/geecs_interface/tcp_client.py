17d2089804a6176e374b7894c53b6d0430ebf02f
guillaumeplateau <126209429+guillaumeplateau@users.noreply.github.com>
2023-02-27 18:34:05 -0800

    Started tcp.client.py

---
+++ GEECS-PythonAPI/geecs_interface/tcp_client.py
@@ -0,0 +1,10 @@
+import socket
+
+
+class TcpClient:
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
