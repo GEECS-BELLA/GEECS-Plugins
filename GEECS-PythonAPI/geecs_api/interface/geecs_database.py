c01bd82d61c7514fc39556af05d7213cda48d949
guillaumeplateau <126209429+guillaumeplateau@users.noreply.github.com>
2023-03-01 18:17:45 -0800

    Robustness added to geecs_database

--- GEECS-PythonAPI/geecs_api/interface/geecs_database.py
+++ GEECS-PythonAPI/geecs_api/interface/geecs_database.py
@@ -2,24 +2,38 @@ import os
 import configparser
 import mysql.connector
 from geecs_errors import *
+import tkinter as tk
+from tkinter import filedialog
+
+# Pop-ups initialization
+tk_root = tk.Tk()
+tk_root.withdraw()
 
 
 def find_database():
-    found = False
-    while not found:
-        os.chdir('../..')
-        found = os.path.exists('user data')
+    default_path = r'C:\GEECS\user data'
+    default_name = 'Configurations.INI'
+
+    if not os.path.isfile(os.path.join(default_path, default_name)):
+        path_cfg = filedialog.askopenfilename(filetypes=[('INI Files', '*.INI'), ('All Files', '*.*')],
+                                              initialdir=default_path,
+                                              initialfile=default_name,
+                                              title='Choose a configuration file:')
+    else:
+        path_cfg = os.path.join(default_path, default_name)
+
+    if not path_cfg:
+        return False
 
     try:
-        os.chdir('user data')
         config = configparser.ConfigParser()
-        config.read('Configurations.INI')
+        config.read(path_cfg)
 
         db_ip = config['Database']['ipaddress']
         db_user = config['Database']['user']
         db_pwd = config['Database']['password']
     except ErrorAPI():
-        db_ip = db_user = db_pwd = 'boo'
+        db_ip = db_user = db_pwd = ''
 
     return db_ip, db_user, db_pwd
 
@@ -28,7 +42,7 @@ class GeecsDatabase:
     database_ip, database_user, database_pwd = find_database()
 
     @staticmethod
-    def lookup_database(dev_name=''):
+    def find_device(dev_name=''):
         try:
             db = mysql.connector.connect(
                 host=GeecsDatabase.database_ip,
@@ -47,13 +61,13 @@ class GeecsDatabase:
             db_result = list(db_cursor.fetchall()[0])
 
             dev_ip = db_result[0]
-            dev_tcp_port = int(db_result[1])
+            dev_port = int(db_result[1])
 
         except ErrorAPI('Device not found in database!'):
-            dev_ip = dev_tcp_port = ''
+            dev_ip = dev_port = ''
             pass
 
-        return dev_ip, dev_tcp_port
+        return dev_ip, dev_port
 
 
 if __name__ == '__main__':
