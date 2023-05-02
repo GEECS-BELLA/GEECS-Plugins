Fixed NI vision png reader. Incorporated changes in Camera averaging functions

GEECS-PythonAPI/geecs_api/tools/images/ni_vision.py
@@ -1,20 +1,35 @@
 import os
-import re
+import png
 import cv2
+import numpy as np
 import numpy.typing as npt
 from typing import Optional
+from pathlib import Path
 from geecs_api.api_defs import SysPath
 
 
-def read_imaq_image(file_path: SysPath, data_type: Optional[str] = None) -> tuple[Optional[npt.ArrayLike], str]:
+def read_imaq_image(file_path: SysPath) -> Optional[npt.ArrayLike]:
     if not os.path.isfile(file_path):
-        return None, ''
+        return None
 
-    if not data_type:
-        image = cv2.imread(file_path, cv2.IMREAD_ANYDEPTH)  # retrieves bit-depth automatically
-        data_type: str = image.dtype.name
+    try:
+        png_header = read_png_header(file_path)
+        image = cv2.imread(file_path, cv2.IMREAD_ANYDEPTH)
 
-    n_bytes: int = int(float(re.search('[0-9]+$', data_type)[0]) / 8)
-    image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) / n_bytes
+        if Path(file_path).suffix.lower() == '.png':
+            data_bytes: int = image.dtype.itemsize
+            return np.right_shift(image, data_bytes * 8 - ord(png_header[b'sBIT']))
+        else:
+            return image
 
-    return image.astype(data_type), data_type
+    except Exception:
+        return None
+
+
+def read_png_header(file_path: SysPath) -> dict[bytes, bytes]:
+    try:
+        png_reader = png.Reader(file_path)
+        return {key: val for key, val in png_reader.chunks() if key != b'IDAT'}
+
+    except Exception:
+        return {}

dataanalysis-notebook/functions/pngTools.py
@@ -4,13 +4,21 @@ Created on Mon Aug 12 22:24:29 2019
 
 @author: ATAP
 """
-import sys
-sys.path.append('..')
-import png #this package does low level i/o on pngs
-import numpy as np
-import imageio as im
 
-def nBitPNG(fname):
+import os
+import sys
+import png  # this package does low level i/o on pngs
+import numpy as np
+import cv2
+import imageio as im
+import numpy.typing as npt
+from typing import Optional, Union
+from pathlib import Path
+
+sys.path.append('..')
+
+
+def nBitPNG(f_name):
     """
     This reads in a png and scales pixel values to compensate for variable bit 
     depth. This is an issue for 12 bit pngs created by NI Vision, which get 
@@ -19,49 +27,74 @@ def nBitPNG(fname):
     analysis package seems to do it natively.
     Based on Kei's "f12bitPngOpnV04" MATLAB function.
     INPUTS:
-    fname: <str> path to png
+    f_name: <str> path to png
     OUTPUTS:
     scaled_image: <2-D float numpy array> image scaled according to significant bits
     """
     
     # get significant bits (try exception handling here)
     try:
-        sBIT = chunkBytes(fname,b'sBIT')
-        sig_bits = int.from_bytes(sBIT,'little')
-#    except ChunkErrorsBit:
-#        print('No sBIT chunk in ' + fname)
-#        sig_bits = 16
-    except: 
-        print('Problem reading chunks in ' + fname)
+        sBIT = chunkBytes(f_name, b'sBIT')
+        sig_bits = int.from_bytes(sBIT, 'little')
+
+    except Exception:
+        print('Problem reading chunks in ' + f_name)
         sig_bits = 16
     
     # read png and scale
-    raw_png = im.imread(fname,as_gray=True)
+    raw_png = im.imread(f_name, as_gray=True)
     scaled_image = raw_png/(2**(16-sig_bits))
     
     return scaled_image
 
-def chunkBytes(fname,chunk_name):
+
+def chunkBytes(f_name, chunk_name):
     """
     Looks for a named chunk in a png and returns its value if found. There is 
     no exception handling if the named chunk isn't found, in which case an 
     error is thrown.\n
     INPUTS\n
-    fname: <str> path to png\n
+    f_name: <str> path to png\n
     chunk_name: <bytes>(e.g. b'sBIT') chunk name\n
     OUTPUTS\n
     chunk_val: <bytes>(e.g. b'sBIT') value of named chunk, if found\n
     """
-    chunkread = png.Reader(filename=fname)
+
+    chunk_reader = png.Reader(filename=f_name)
     
     curr_name = b'turd'
     while curr_name != chunk_name:
-    #while chunkname != b'IEND':
-        
         if curr_name == b'IEND':
-            chunk_val=b'\x00'
+            chunk_val = b'\x00'
             break
         else:
-            curr_name,chunk_val = chunkread.chunk()
+            curr_name, chunk_val = chunk_reader.chunk()
     
     return chunk_val
+
+
+def read_imaq_image(file_path: Union[str, bytes, os.PathLike]) -> Optional[npt.ArrayLike]:
+    if not os.path.isfile(file_path):
+        return None
+
+    try:
+        png_header = read_png_header(file_path)
+        image = cv2.imread(file_path, cv2.IMREAD_ANYDEPTH)
+
+        if Path(file_path).suffix.lower() == '.png':
+            data_bytes: int = image.dtype.itemsize
+            return np.right_shift(image, data_bytes * 8 - ord(png_header[b'sBIT']))
+        else:
+            return image
+
+    except Exception:
+        return None
+
+
+def read_png_header(file_path: Union[str, bytes, os.PathLike]) -> dict[bytes, bytes]:
+    try:
+        png_reader = png.Reader(file_path)
+        return {key: val for key, val in png_reader.chunks() if key != b'IDAT'}
+
+    except Exception:
+        return {}