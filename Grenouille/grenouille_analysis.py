2977e85b955d82c7af3532a8783b9da2c7339856
TAU-Reinier <reinier.vanmourik@tausystems.com>
2023-06-29 14:40:33 -0700

    Create Grenouille python package

---
+++ Grenouille/grenouille_analysis.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 10:44:15 2023
+
+@author: Reinier van Mourik, TAU Systems
+"""
+
+from __future__ import annotations
+
+from pint import UnitRegistry
+ureg = UnitRegistry()
+Q_ = ureg.Quantity
+
