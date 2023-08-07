907f579c2168de77df8c4a39a6bcb946d1d098a5
Christopher Doss <CEDoss@lbl.gov>
2023-08-07 14:48:51 -0700

    A quick script rewrite to make it compatible with the LabView input.

---
+++ ImageAnalysis/HiResMagSpec_Analysis/HiResMagSpec_LabView.py
@@ -0,0 +1,55 @@
+"""
+Mon 8-7-2023
+
+A wrapper function to call my Hi Res Mag Spec Analysis in the framework of the Labview code.
+
+Inputs an image and outputs a list of doubles.
+
+All constants are defined in the function.
+
+@ Chris
+"""
+
+import sys
+from array import array
+sys.path.insert(0, "../")
+import modules.HiResMagSpecAnalysis as MagSpecAnalysis
+import modules.DirectoryModules as DirectoryFunc
+
+def HiResMagSpec_LabView(image, step):
+
+    # Factor to go from camera counts to pC/MeV
+    # Record: July 25th, Scan 24, HiResMagSpec
+    normalization_triggerdelay = 15.497208
+    normalization_exposure = 0.010000
+    normalization_factor = 1.1301095153900242e-06
+
+    inputParams = {
+        "Threshold-Value": 230,                         # Large enough to remove noise level
+        "Saturation-Value": 4095,                       # Just below the maximum for 2^12
+        "Normalization-Factor": normalization_factor,   # Only valid for the above camera settings
+        "Transverse-Calibration": 43,                   # ums / pixel
+        "Transverse-Slice-Threshold": 0.02,
+        "Transverse-Slice-Binsize": 5
+    }
+
+    MagSpecDict = MagSpecAnalysis.AnalyzeImage(image, inputParams)
+    values = array('d', MagSpecDict.values())
+    return values
+
+
+if __name__ == '__main__':
+
+    data_day = 29
+    data_month = 6
+    data_year = 2023
+    scan_number = 23
+    shot_number = 20
+
+    superpath = DirectoryFunc.CompileDailyPath(data_day, data_month, data_year)
+    image_name = "U_HiResMagCam"
+
+    raw_image = MagSpecAnalysis.LoadImage(superpath, scan_number, shot_number, image_name)
+
+    results = HiResMagSpec_LabView(raw_image, shot_number)
+    print(results)
