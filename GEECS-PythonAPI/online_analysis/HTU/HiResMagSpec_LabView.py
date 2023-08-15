2392476f58c08e3cffc22f3640ddd35bd71e29ff
Christopher Doss <CEDoss@lbl.gov>
2023-08-15 10:06:41 -0700

    Initial merge of my files onto this separate branch for MagSpec analysis.

--- GEECS-PythonAPI/online_analysis/HTU/HiResMagSpec_LabView.py
+++ GEECS-PythonAPI/online_analysis/HTU/HiResMagSpec_LabView.py
@@ -3,7 +3,9 @@ Mon 8-7-2023
 
 A wrapper function to call my Hi Res Mag Spec Analysis in the framework of the Labview code.
 
-Inputs an image and outputs a list of doubles.
+Inputs an image and outputs a list of doubles. For the "_LabView" function.
+
+The "_Dictionary" function is the same, just the list of doubles is instead a dictionary
 
 All constants are defined in the function.
 
@@ -11,12 +13,22 @@ All constants are defined in the function.
 """
 
 from array import array
+import numpy as np
+import time
 
 import HiResAnalysisModules.HiResMagSpecAnalysis as MagSpecAnalysis
+import HiResAnalysisModules.HiResMagSpecPlotter as MagPlotter
 import HiResAnalysisModules.DirectoryModules_HiRes as DirectoryFunc
-#import HiResAnalysisModules.pngTools_HiRes as pngTools
+import HiResAnalysisModules.pngTools_HiRes as pngTools
+
 
 def HiResMagSpec_LabView(image):
+    returned_image, MagSpecDict, inputParams = HiResMagSpec_Dictionary(image)
+    values = array('d', MagSpecDict.values())
+    return (returned_image, list(values))
+
+
+def HiResMagSpec_Dictionary(image):
 
     # Factor to go from camera counts to pC/MeV
     # Record: July 25th, Scan 24, HiResMagSpec
@@ -25,34 +37,48 @@ def HiResMagSpec_LabView(image):
     normalization_factor = 1.1301095153900242e-06
 
     inputParams = {
-        "Threshold-Value": 230,                         # Large enough to remove noise level
+        "Threshold-Value": 100,#230,                    # Large enough to remove noise level
+        "Pixel-Crop": 1,                                # Number of edge pixels to crop
         "Saturation-Value": 4095,                       # Just below the maximum for 2^12
         "Normalization-Factor": normalization_factor,   # Only valid for the above camera settings
         "Transverse-Calibration": 43,                   # ums / pixel
+        "Do-Transverse-Calculation": True,              # False if you want to skip
         "Transverse-Slice-Threshold": 0.02,
         "Transverse-Slice-Binsize": 5
     }
+    processed_image = image.astype(np.float32)
+    returned_image, MagSpecDict = MagSpecAnalysis.AnalyzeImage(processed_image, inputParams)
+    unnormalized_image = returned_image / normalization_factor
+    uint_image = unnormalized_image.astype(np.uint16)
+    return uint_image, MagSpecDict, inputParams
 
-    MagSpecDict = MagSpecAnalysis.AnalyzeImage(image, inputParams)
-    values = array('d', MagSpecDict.values())
-    return (image,list(values))
 
-
-"""
 if __name__ == '__main__':
 
-    data_day = 29
-    data_month = 6
+    data_day = 9#29#9
+    data_month = 8#6#8
     data_year = 2023
-    scan_number = 23
-    shot_number = 20
+    scan_number = 9#23#9
+    shot_number = 28
 
     superpath = DirectoryFunc.CompileDailyPath(data_day, data_month, data_year)
-    image_name = "U_HiResMagCam"
+    image_name = "UC_TestCam"
+    #image_name = "U_HiResMagCam"
 
     fullpath = DirectoryFunc.CompileFileLocation(superpath, scan_number, shot_number, image_name, suffix=".png")
     raw_image = pngTools.nBitPNG(fullpath)
 
-    results = HiResMagSpec_LabView(raw_image)
-    print(results)
-"""
+    start = time.perf_counter()
+    returned_image, analyzeDict, inputParams = HiResMagSpec_Dictionary(raw_image)
+    print("Elapsed Time: ", time.perf_counter()-start, "s")
+    print(analyzeDict)
+
+    num_pixel = inputParams["Pixel-Crop"]
+    raw_image = raw_image[num_pixel:-num_pixel, num_pixel:-num_pixel]
+    doThreshold = True
+    doNormalization = True
+
+    plotInfo = DirectoryFunc.CompilePlotInfo(data_day, data_month, data_year, scan_number, shot_number, "U_HiResMagSpec")
+    MagPlotter.PlotEnergyProjection(raw_image, analyzeDict, inputParams, plotInfo=plotInfo, doThreshold=doThreshold, doNormalize=doNormalization)
+    MagPlotter.PlotSliceStatistics(raw_image, analyzeDict, inputParams, plotInfo=plotInfo, doThreshold=doThreshold, doNormalize=doNormalization)
+    MagPlotter.PlotBeamDistribution(raw_image, analyzeDict, inputParams, plotInfo=plotInfo, doThreshold=doThreshold, doNormalize=doNormalization, style=2)
--- GEECS-PythonAPI/online_analysis/HTU/OnlineAnalysisModules/HiResMagSpecAnalysis.py
+++ GEECS-PythonAPI/online_analysis/HTU/OnlineAnalysisModules/HiResMagSpecAnalysis.py
@@ -17,21 +17,25 @@ import HiResAnalysisModules.CedossMathTools_HiRes as MathTools
 import HiResAnalysisModules.EnergyAxisLookup_HiRes as EnergyAxisLookup
 
 
-def PrintTime(label, time_in, doPrint = False):
+def PrintTime(label, time_in, doPrint=False):
     if doPrint:
-        print(label, time.perf_counter()-time_in)
+        print(label, time.perf_counter() - time_in)
     return time.perf_counter()
 
-def AnalyzeImage(image, inputParams):
+
+def AnalyzeImage(inputImage, inputParams):
     currentTime = time.perf_counter()
     doPrint = False
 
+    numPixelCrop = inputParams["Pixel-Crop"]
+    roiImage = inputImage[numPixelCrop: -numPixelCrop, numPixelCrop: -numPixelCrop]
+
     saturationValue = inputParams["Saturation-Value"]
-    saturationCheck = SaturationCheck(image, saturationValue)
+    saturationCheck = SaturationCheck(roiImage, saturationValue)
     currentTime = PrintTime(" Saturation Check:", currentTime, doPrint=doPrint)
 
     threshold = inputParams["Threshold-Value"]
-    image = ThresholdReduction(image, threshold)
+    image = ThresholdReduction(roiImage, threshold)
     currentTime = PrintTime(" Threshold Subtraction", currentTime, doPrint=doPrint)
 
     normalizationFactor = inputParams["Normalization-Factor"]
@@ -78,45 +82,53 @@ def AnalyzeImage(image, inputParams):
         peakChargeEnergy = CalculatePeakEnergy(charge_arr, energy_arr)
         currentTime = PrintTime(" Energy at Peak Charge:", currentTime, doPrint=doPrint)
 
-        # binsize = inputParams["Transverse-Slice-Binsize"]
-        # sliceThreshold = inputParams["Transverse-Slice-Threshold"]
-        # sigma_arr, x0_arr, amp_arr, err_arr = FitTransverseGaussianSlices(image, calibrationFactor=calibrationFactor,
-                                                                          # threshold=sliceThreshold, binsize=binsize)
-        # currentTime = PrintTime(" Gaussian Fits for each Slice:", currentTime, doPrint=doPrint)
+        doTransverse = inputParams["Do-Transverse-Calculation"]
+        if doTransverse:
+            binsize = inputParams["Transverse-Slice-Binsize"]
+            sliceThreshold = inputParams["Transverse-Slice-Threshold"]
+            sigma_arr, x0_arr, amp_arr, err_arr = TransverseSliceLoop(image, calibrationFactor=calibrationFactor,
+                                                                      threshold=sliceThreshold, binsize=binsize)
+            currentTime = PrintTime(" Gaussian Fits for each Slice:", currentTime, doPrint=doPrint)
 
-        # averageBeamSize = CalculateAverageSize(sigma_arr, amp_arr)
-        # currentTime = PrintTime(" Average Beam Size:", currentTime, doPrint=doPrint)
+            averageBeamSize = CalculateAverageSize(sigma_arr, amp_arr)
+            currentTime = PrintTime(" Average Beam Size:", currentTime, doPrint=doPrint)
 
-        # linear_fit = FitBeamAngle(x0_arr, amp_arr, energy_arr)
-        # currentTime = PrintTime(" Beam Angle Fit:", currentTime, doPrint=doPrint)
+            linear_fit = FitBeamAngle(x0_arr, amp_arr, energy_arr)
+            currentTime = PrintTime(" Beam Angle Fit:", currentTime, doPrint=doPrint)
 
-        # beamAngle = linear_fit[0]
-        # beamIntercept = linear_fit[1]
-        # projected_axis, projected_arr, projectedBeamSize = CalculateProjectedBeamSize(image, calibrationFactor)
-        # projectedBeamSize = projectedBeamSize * calibrationFactor
-        # PrintTime(" Projected Size:", currentTime, doPrint=doPrint)
+            beamAngle = linear_fit[0]
+            beamIntercept = linear_fit[1]
+            projected_axis, projected_arr, projectedBeamSize = CalculateProjectedBeamSize(image, calibrationFactor)
+            projectedBeamSize = projectedBeamSize * calibrationFactor
+            PrintTime(" Projected Size:", currentTime, doPrint=doPrint)
+        else:
+            averageBeamSize = float(0.0)
+            projectedBeamSize = float(0.0)
+            beamAngle = float(0.0)
+            beamIntercept = float(0.0)
 
     magSpecDict = {
-        "Clipped-Percentage": clippedPercentage,
-        "Saturation-Counts": saturationCheck,
-        "Charge-On-Camera": chargeOnCamera,
-        "Peak-Charge": peakCharge,
-        "Peak-Charge-Energy": peakChargeEnergy,
-        "Average-Energy": averageEnergy,
-        "Energy-Spread": energySpread,
-        "Energy-Spread-Percent": energySpread/averageEnergy,
-        # "Average-Beam-Size": averageBeamSize,
-        # "Projected-Beam-Size": projectedBeamSize,
-        # "Beam-Tilt": beamAngle,
-        # "Beam-Intercept": beamIntercept,
-        # "Beam-Intercept-100MeV": 100*beamAngle + beamIntercept
+        "Clipped-Percentage": clippedPercentage,  # 1
+        "Saturation-Counts": saturationCheck,  # 2
+        "Charge-On-Camera": chargeOnCamera,  # 3
+        "Peak-Charge": peakCharge,  # 4
+        "Peak-Charge-Energy": peakChargeEnergy,  # 5
+        "Average-Energy": averageEnergy,  # 6
+        "Energy-Spread": energySpread,  # 7
+        "Energy-Spread-Percent": energySpread / averageEnergy,  # 8
+        "Average-Beam-Size": averageBeamSize,
+        "Projected-Beam-Size": projectedBeamSize,
+        "Beam-Tilt": beamAngle,
+        "Beam-Intercept": beamIntercept,
+        "Beam-Intercept-100MeV": 100 * beamAngle + beamIntercept
     }
-    return magSpecDict
+    return image, magSpecDict
 
 
 def NormalizeImage(image, normalizationFactor):
     returnimage = np.copy(image) * normalizationFactor
     return returnimage
+
 
 """
 def PrintNormalization(shotnumber, tdms_filepath):
@@ -142,6 +154,7 @@ def LoadImage(superpath, scannumber, shotnumber, folderpath):
     return image
 """
 
+
 def ThresholdReduction(image, threshold):
     returnimage = np.copy(image) - threshold
     returnimage[np.where(returnimage < 0)] = 0
@@ -149,12 +162,13 @@ def ThresholdReduction(image, threshold):
 
 
 def CalculateClippedPercentage(image):
+    # roi_image = image[1:-1,1:-1]
     clipcheck = np.append(np.append(np.append(image[0, :], image[:, 0]), image[-1, :]), image[:, -1])
     maxval = np.max(image)
     if maxval != 0:
         return np.max(clipcheck) / maxval
     else:
-        return 1.0
+        return 1.1
 
 
 def CalculateProjectedBeamSize(image, calibrationFactor):
@@ -216,6 +230,7 @@ def FitBeamAngle(x0_arr, amp_arr, energy_arr):
     linear_fit = np.polyfit(energy_arr, x0_arr, deg=1, w=np.power(amp_arr, 2))
     return linear_fit
 
+
 """
 def GetBeamCharge(tdms_filepath):
     # For future reference, can list all of the groups using tdms_file.groups()
@@ -246,6 +261,7 @@ def GetCameraTriggerAndExposure(tdms_filepath):
     return trigger_list, exposure_list
 """
 
+
 def FindMax(image):
     ymax, xmax = np.unravel_index(np.argmax(image), image.shape)
     maxval = image[ymax, xmax]
@@ -255,8 +271,10 @@ def FindMax(image):
 def SaturationCheck(image, saturationValue):
     return len(np.where(image > saturationValue)[0])
 
+
 def Gaussian(x, amp, sigma, x0):
     return amp * np.exp(-0.5 * ((x - x0) / sigma) ** 2)
+
 
 def FitDataSomething(data, axis, function, guess=[0., 0., 0.]):
     errfunc = lambda p, x, y: function(x, *p) - y
@@ -264,7 +282,9 @@ def FitDataSomething(data, axis, function, guess=[0., 0., 0.]):
     p1, success = optimize.leastsq(errfunc, p0[:], args=(axis, data))
     return p1
 
-def FitTransverseGaussianSlices(image, calibrationFactor=1, threshold=0.01, binsize=1):
+
+def TransverseSliceLoop(image, calibrationFactor=1, threshold=0.01, binsize=1, option=1):
+    # option 0 for Gaussian fits, option 1 for moment statistics.  1 is usually super good enough
     ny, nx = np.shape(image)
     xloc, yloc, maxval = FindMax(image)
 
@@ -273,36 +293,54 @@ def FitTransverseGaussianSlices(image, calibrationFactor=1, threshold=0.01, bins
     x0_arr = np.zeros(nx)
     amp_arr = np.zeros(nx)
 
-    for i in range(int(nx/binsize)):
+    for i in range(int(nx / binsize)):
         if binsize == 1:
             slice_arr = image[:, i]
         else:
-            slice_arr = np.average(image[:, binsize*i:(binsize*i)+binsize-1], axis=1)
+            slice_arr = np.average(image[:, binsize * i:(binsize * i) + binsize - 1], axis=1)
         if np.max(slice_arr) > threshold * maxval:
             axis_arr = np.linspace(0, len(slice_arr), len(slice_arr)) * calibrationFactor
             axis_arr = np.flip(axis_arr)
 
-            fit = FitDataSomething(slice_arr, axis_arr, Gaussian,
-                                   guess=[max(slice_arr), 5 * calibrationFactor,
-                                          axis_arr[np.argmax(slice_arr)]])
-            amp_fit, sigma_fit, x0_fit = fit
-            amp_arr[binsize*i:binsize*(i+1)] = amp_fit
-            sigma_arr[binsize*i:binsize*(i+1)] = sigma_fit
-            x0_arr[binsize*i:binsize*(i+1)] = x0_fit
+            if option == 0:
+                slice_sigma, slice_x0, slice_amp, slice_err = FitTransverseGaussianSlices(axis_arr, slice_arr)
+            if option == 1:
+                slice_sigma, slice_x0, slice_amp, slice_err = GetTransverseStatSlices(axis_arr, slice_arr)
 
-            # Check to see that our x0 is within bounds (only an issue for vertically-clipped beams)
-            if x0_arr[binsize*i] < axis_arr[-1] or x0_arr[binsize*i] > axis_arr[0]:
-                sigma_arr[binsize*i:binsize*(i+1)] = 0
-                x0_arr[binsize*i:binsize*(i+1)] = 0
-                amp_arr[binsize*i:binsize*(i+1)] = 0
-                err_arr[binsize*i:binsize*(i+1)] = 0
-            else:
-                func = Gaussian(axis_arr, *fit)
-                error = np.sum(np.square(slice_arr - func))
-                err_arr[binsize*i:binsize*(i+1)] = np.sqrt(error) * 1e3
         else:
-            sigma_arr[binsize*i:binsize*(i+1)] = 0
-            x0_arr[binsize*i:binsize*(i+1)] = 0
-            amp_arr[binsize*i:binsize*(i+1)] = 0
-            err_arr[binsize*i:binsize*(i+1)] = 0
+            slice_sigma = 0
+            slice_x0 = 0
+            slice_amp = 0
+            slice_err = 0
+        sigma_arr[binsize * i:binsize * (i + 1)] = slice_sigma
+        x0_arr[binsize * i:binsize * (i + 1)] = slice_x0
+        amp_arr[binsize * i:binsize * (i + 1)] = slice_amp
+        err_arr[binsize * i:binsize * (i + 1)] = slice_err
     return sigma_arr, x0_arr, amp_arr, err_arr
+
+
+def GetTransverseStatSlices(axis_arr, slice_arr):
+    amp_slice = np.average(slice_arr)
+    x0_slice = np.average(np.average(axis_arr, weights=slice_arr))
+    sigma_slice = np.sqrt(np.average(np.power(axis_arr - x0_slice, 2), weights=slice_arr))
+    err_slice = 0
+    return sigma_slice, x0_slice, amp_slice, err_slice
+
+
+def FitTransverseGaussianSlices(axis_arr, slice_arr):
+    fit = FitDataSomething(slice_arr, axis_arr, Gaussian,
+                           guess=[max(slice_arr), 5 * 43,  # calibrationFactor,
+                                  axis_arr[np.argmax(slice_arr)]])
+    amp_fit, sigma_fit, x0_fit = fit
+
+    # Check to see that our x0 is within bounds (only an issue for vertically-clipped beams)
+    if x0_fit < axis_arr[-1] or x0_fit > axis_arr[0]:
+        sigma_fit = 0
+        x0_fit = 0
+        amp_fit = 0
+        err_fit = 0
+    else:
+        func = Gaussian(axis_arr, *fit)
+        error = np.sum(np.square(slice_arr - func))
+        err_fit = np.sqrt(error) * 1e3
+    return sigma_fit, x0_fit, amp_fit, err_fit
