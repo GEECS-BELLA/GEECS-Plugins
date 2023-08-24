8fe9510956b5906a8c6cd22d36e4455818126ad0
skbarber <64565579+skbarber@users.noreply.github.com>
2023-08-24 09:10:39 -0700

    Update image_wrapper_functions.py
    added "LabviewGEECSImageAnalyzer" which becomes a base class for online image analysis using the GEECs driver. it should only handle things specific to the labview implementation. One method added so far is a pre-processing step that makes use of the background image produced by labview. This can be used or ignored. I would like to add another method in this base class to "simulate" images to help expedite development of new analysis routines. Using a simulate case allows for full testing of the labview/python integration and gives the use access to using the existing camera GUI for inspecting the processed images to make sure things are as they should be.

--- ImageAnalysis/image_analysis/image_wrapper_functions.py
+++ ImageAnalysis/image_analysis/image_wrapper_functions.py
@@ -1,6 +1,51 @@
 import numpy as np
 
-def HiResMagSpec_LabView(image):
+class LabviewGEECSImageAnalyzer:
+    """
+    Base class for GEECS Image Analyzers for LabView driver. This should
+    only handle things specific to using the labview driver. For example,
+    ensuring the input parameters/output parameters conform to how they 
+    are implemented on the labview side.
+    
+    """
+
+    def __init__(self, subtract_background=False):
+        self.subtract_background = subtract_background
+
+    def preprocess_image(self, image, background=None):
+        """Preprocess the image before analysis."""
+        if self.subtract_background and background is not None:
+            if image.shape != background.shape:
+                raise ValueError("Image and background shapes do not match.")
+            return image - background
+        return image
+
+    def do_analysis(self, image, background=None):
+        """Virtual method to be overridden by specific device implementations."""
+        processed_image = self.preprocess_image(image, background)
+        raise NotImplementedError("The do_analysis method should be implemented in subclasses.")
+
+    def validate_output(self, result):
+        """
+        Validates and/or provides default values for the output data.
+        """
+        # Default outputs
+        default_2d_uint16 = np.zeros((2, 2), dtype=np.uint16)
+        default_1d_float64 = np.zeros(2, dtype=np.float64)
+        default_2d_float64 = np.zeros((2, 2), dtype=np.float64)
+
+        if not (isinstance(result[0], np.ndarray) and result[0].dtype == np.uint16):
+            result = (default_2d_uint16, result[1], result[2])
+
+        if not (isinstance(result[1], np.ndarray) and len(result[1].shape) == 1 and result[1].dtype == np.float64):
+            result = (result[0], default_1d_float64, result[2])
+
+        if not (isinstance(result[2], np.ndarray) and len(result[2].shape) == 2 and result[2].dtype == np.float64):
+            result = (result[0], result[1], default_2d_float64)
+
+        return result
+
+class HiResMagSpec(LabviewGEECSImageAnalyzer):
     """
     Mon 8-7-2023
 
@@ -16,54 +61,64 @@ def HiResMagSpec_LabView(image):
 
     @ Chris
     """
-    
-    #import the analysis class for the specific camera device instance
-    from analyzers.U_HiResMagCam.U_HiResMagSpec import U_HiResMagSpecImageAnalyzer as U_HiResMagSpecImageAnalyzer
-    returned_image, MagSpecDict, inputParams = U_HiResMagSpecImageAnalyzer().analyze_image(image)
-   
-    values = np.array([
-                MagSpecDict["Clipped-Percentage"],
-                MagSpecDict["Saturation-Counts"],
-                MagSpecDict["Charge-On-Camera"],
-                MagSpecDict["Peak-Charge"],
-                MagSpecDict["Peak-Charge-Energy"],
-                MagSpecDict["Average-Energy"],
-                MagSpecDict["Energy-Spread"],
-                MagSpecDict["Energy-Spread-Percent"],
-                MagSpecDict["Average-Beam-Size"],
-                MagSpecDict["Projected-Beam-Size"],
-                MagSpecDict["Beam-Tilt"],
-                MagSpecDict["Beam-Intercept"],
-                MagSpecDict["Beam-Intercept-100MeV"]
-            ])
-   
-    return (returned_image, list(values))
-    
-    
+    def __init__(self):
+        super().__init__(subtract_background=True)
+
+    def do_analysis(self, image, background=None):
+        """Perform analysis specific to HiResMagSpec."""
+        processed_image = self.preprocess_image(image, background)
+        
+        # Local import for device-specific analysis
+        from analyzers.U_HiResMagCam.U_HiResMagSpec import U_HiResMagSpecImageAnalyzer
+        returned_image, MagSpecDict, _ = U_HiResMagSpecImageAnalyzer().analyze_image(processed_image)
+
+        # Define the keys for which values need to be extracted
+        keys_of_interest = [
+            "Clipped-Percentage",
+            "Saturation-Counts",
+            "Charge-On-Camera",
+            "Peak-Charge",
+            "Peak-Charge-Energy",
+            "Average-Energy",
+            "Energy-Spread",
+            "Energy-Spread-Percent",
+            "Average-Beam-Size",
+            "Projected-Beam-Size",
+            "Beam-Tilt",
+            "Beam-Intercept",
+            "Beam-Intercept-100MeV"
+        ]
+
+        values = np.array([MagSpecDict[key] for key in keys_of_interest]).astype(np.float64)
+        
+        result=(returned_image, values, np.zeros((2, 2), dtype=np.float64))
+        
+        return result
+
+# Dictionary to map device types to their respective analysis functions
+DEVICE_FUNCTIONS = {
+    "UC_TestCam": HiResMagSpec(),
+    # Add more device types as needed...
+}
+
 def execute(device_type, image, background):
-    """
-    Main function to analyze an image based on the device type.
-
-    Parameters:
-    -----------
-    device_type : str
-        Type of the device, e.g., "UC_TestCam". This is used to 
-        select out the specific device using if/else statement.
-        to do: switch to use a dictionary
-    image : numpy.ndarray
-        The image to be analyzed.
-    background : numpy.ndarray
-        The background to be subtracted or used in analysis.
-
-    Returns:
-    --------
-    tuple
-        Analysis result data as a tuple containing:
-        - 2D uint16 array
-        - 1D double array
-        - 2D double array
-
-    """
-    if device_type=="UC_TestCam":
-        result=HiResMagSpec_LabView(image)
-        return result[0],result[1],np.zeros((2, 2), dtype=np.float64)
+    """Main execution function to handle image analysis based on device type."""
+    device = DEVICE_FUNCTIONS.get(device_type)
+    
+    if device:
+        result = device.do_analysis(image, background)
+        validated_result = device.validate_output(result)
+        return validated_result[0], validated_result[1], validated_result[2]
+    else:
+        raise ValueError(f"Unknown device type: {device_type}")
+        
+# def execute(device_type, input_tuple):
+    # """Main execution function to handle image analysis based on device type."""
+    # device = DEVICE_FUNCTIONS.get(device_type)
+    
+    # if device:
+        # result = device.do_analysis(np.array(input_tuple[0]), np.array(input_tuple[1]))
+        # validated_result = device.validate_output(result)
+        # return validated_result[0], validated_result[1], validated_result[2]
+    # else:
+        # raise ValueError(f"Unknown device type: {device_type}")
