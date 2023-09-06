8d09c82851a8c104c0106f036c65d1b04c285c94
Christopher Doss <CEDoss@lbl.gov>
2023-09-06 16:43:00 -0700

    Began some analysis of the ACave Mag Spec's energy axis.  Also started analysis of UndulatorExitCam by performing a calibration on the test images.

---
+++ ImageAnalysis/image_analysis/analyzers/UC_UndulatorExitCam.py
@@ -0,0 +1,10 @@
+"""
+Wednesday 9-6-2023
+
+Will eventually flesh this out more.  Just writing out what I want the analysis to do.  Mostly just rotate the image
+and then do a projection.  Then turn this projection into a spectra.
+
+@Chris
+"""
+
+print("Placeholder!")
