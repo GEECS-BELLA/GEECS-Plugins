d272786d43361951fd46aba38724bcc0e49a3a29
guillaumeplateau <126209429+guillaumeplateau@users.noreply.github.com>
2023-09-11 00:55:10 -0700

    Testing possible weighting and fitting of objective function

--- GEECS-PythonAPI/labview_interface/HTU/htu_classes.py
+++ GEECS-PythonAPI/labview_interface/HTU/htu_classes.py
@@ -106,7 +106,7 @@ class LPA:
 
         return cancel, scan_folder
 
-    def z_scan_analysis(self, exp: Experiment, scan_path: Path) -> tuple[dict[str, dict[str, np.ndarray]], np.ndarray]:
+    def z_scan_analysis(self, exp: Experiment, scan_path: Path) -> tuple[dict[str, dict[str, np.ndarray]], np.ndarray, np.ndarray]:
         if self.jet is None:
             device = 'U_ESP_JetXYZ'
             variable = 'Position.Axis 3'
@@ -124,9 +124,11 @@ class LPA:
         obj_2 = obj_2 / np.max(obj_2)
         obj_3 = magspec_data['spec_hres_stats']['med_peak_fit_MeV']
         obj_3 = 1 - np.abs(obj_3 / 100. - 1)
-        objective = obj_1 * obj_2 * obj_3
+        # objective = obj_1 * obj_2 * obj_3
+        # objective = obj_1 * obj_2**2 * obj_3**2
+        objective = (obj_1 + 3 * obj_2 + 2 * obj_3) / 9
 
-        return magspec_data, objective
+        return magspec_data, setpoints, objective
 
 
 if __name__ == "__main__":
@@ -134,7 +136,14 @@ if __name__ == "__main__":
     _scan_path = Path(r'C:\Users\GuillaumePlateau\Documents\LBL\Data\Undulator\Y2023\07-Jul\23_0706\scans\Scan004')
 
     lpa = LPA(_htu.is_offline)
-    lpa.z_scan_analysis(_htu, _scan_path)
+    _, _setpoints, _objective = lpa.z_scan_analysis(_htu, _scan_path)
+    _obj_fit = np.polyfit(_setpoints, _objective, 4)
+    _fit_x = np.linspace(_setpoints[0], _setpoints[-1], 100)
+    _fit_y = np.polyval(_obj_fit, _fit_x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(_setpoints, _objective)
+    plt.plot(_fit_x, _fit_y)
+    plt.show(block=True)
 
     # _scan_data = ScanData(_scan_path, ignore_experiment_name=_htu.is_offline)
     # _indexes, _setpoints, _matching = _scan_data.bin_data('U_ESP_JetXYZ', 'Position.Axis 3')
