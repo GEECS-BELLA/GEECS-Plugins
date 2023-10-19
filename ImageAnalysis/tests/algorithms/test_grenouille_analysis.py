334a2c2fa34f50129096ea785d046fcf23480060
TAU-Reinier <reinier.vanmourik@tausystems.com>
2023-10-19 15:43:47 -0700

    Rename E_time_step, E_t, and E to pulse_time_step, pulse_t, and pulse_E

--- ImageAnalysis/tests/algorithms/test_grenouille_analysis.py
+++ ImageAnalysis/tests/algorithms/test_grenouille_analysis.py
@@ -4,7 +4,7 @@ import unittest
 
 import numpy as np
 
-from image_analysis.algorithms.grenouille import GrenouilleRetrieval
+from image_analysis.algorithms.grenouille import GrenouilleRetrieval, create_test_grenouille_trace
 from image_analysis import ureg, Q_
 
 class GrenouilleAnalysisTestCase(unittest.TestCase):
@@ -14,17 +14,17 @@ class GrenouilleAnalysisTestCase(unittest.TestCase):
         gr = GrenouilleRetrieval(calculate_next_E_method='integration')
 
         # ## Calculate sample grenouille trace from example E field pulse
-        E_time_step = Q_(5, 'fs')
-        E_t = (np.arange(127) - 64) * E_time_step
-        E = np.exp(-1/2 * np.square(E_t) / Q_(30, 'fs')**2) * np.exp(1j * E_t * 2*np.pi / Q_(200, 'fs'))
-        E = E.m_as('')
+        pulse_time_step = Q_(5, 'fs')
+        pulse_t = (np.arange(127) - 64) * pulse_time_step
+        pulse_E = np.exp(-1/2 * np.square(pulse_t) / Q_(30, 'fs')**2) * np.exp(1j * pulse_t * 2*np.pi / Q_(200, 'fs'))
+        pulse_E = pulse_E.m_as('')
 
         grenouille_trace_center_wavelength = Q_(400, 'nm')
         grenouille_trace_wavelength_step = Q_(0.7, 'nm')
         grenouille_trace_time_delay_step = Q_(2.1, 'fs')
         grenouille_trace_shape = (80, 81)
 
-        grenouille_trace = gr.simulate_grenouille_trace(E, E_time_step, grenouille_trace_shape, 
+        grenouille_trace = gr.simulate_grenouille_trace(pulse_E, pulse_time_step, grenouille_trace_shape, 
                                grenouille_trace_center_wavelength=grenouille_trace_center_wavelength, 
                                grenouille_trace_wavelength_step=grenouille_trace_wavelength_step, 
                                grenouille_trace_time_delay_step=grenouille_trace_time_delay_step,
@@ -39,8 +39,8 @@ class GrenouilleAnalysisTestCase(unittest.TestCase):
                                          time_delay_step=grenouille_trace_time_delay_step,
                                         )
 
-        E_recovered_at_E_t = np.interp(E_t,  gr.E_t, E_recovered, left=0.0, right=0.0)
-        self.assertLess(np.max(np.abs(E_recovered_at_E_t - E)), 0.05)
+        E_recovered_at_E_t = np.interp(pulse_t,  gr.E_t, E_recovered, left=0.0, right=0.0)
+        self.assertLess(np.max(np.abs(E_recovered_at_E_t - pulse_E)), 0.05)
 
         # ## check generalized_projection/along_gradient method
         gr.calculate_next_E_method = 'generalized_projection/along_gradient'
@@ -49,8 +49,8 @@ class GrenouilleAnalysisTestCase(unittest.TestCase):
                                         grenouille_trace_wavelength_step=grenouille_trace_wavelength_step, 
                                         time_delay_step=grenouille_trace_time_delay_step,
                                         )
-        E_recovered_at_E_t = np.interp(E_t,  gr.E_t, E_recovered, left=0.0, right=0.0)
-        self.assertLess(np.max(np.abs(E_recovered_at_E_t - E)), 0.05)
+        E_recovered_at_E_t = np.interp(pulse_t,  gr.E_t, E_recovered, left=0.0, right=0.0)
+        self.assertLess(np.max(np.abs(E_recovered_at_E_t - pulse_E)), 0.05)
 
 
 class UtilsTestCase(unittest.TestCase):
