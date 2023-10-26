a9503a99d28511c39fd34fad30d04e89f8b85490
skbarber <64565579+skbarber@users.noreply.github.com>
2023-10-25 23:34:50 -0700

    adding xopt/GEECS gui

---
+++ xopt/backend.py
@@ -0,0 +1,179 @@
+
+import os
+import yaml
+import time
+import numpy as np
+import pandas as pd
+import shelve
+import sys
+
+from xopt.evaluator import Evaluator
+from xopt.generators.bayesian import ExpectedImprovementGenerator
+from xopt import Xopt
+
+import torch
+
+import sys
+import time
+
+from geecs_functions import GeecsXoptInterface
+geecs_interface = GeecsXoptInterface()
+
+
+class MyBackend:
+    def __init__(self):
+        self.devices = {}  # A dict that will store the initialized device controls
+        self.config_params = {
+            'normalize': None,  # default value
+            'opt_method': None,  # default value
+            'opt_steps': None,
+            'shots_per_step':None,
+            'opt_target_device': None,
+            'opt_target_var_name':None
+        }
+        
+        self.bayes_UCB_config = {
+            'generator_name': 'upper_confidence_bound',  # default value
+            'generator_n_initial': 2,  # default value
+            'generator_acq': {'beta':0.1}
+        }
+        
+        self.nelder_config = {
+            'generator_name': 'neldermead',  # default value
+            'generator_xatol': 2,  # default value            
+        }
+        
+        self.yaml_config={}
+        self.X = None
+        
+        #some parameters for setting up the simulation case
+        self.optPosition = np.array([18.45, 0.6])
+        self.numParts = 200000
+
+        self.startDist = np.transpose([
+            np.random.normal(self.optPosition[0], 0.4, self.numParts),
+            np.random.normal(self.optPosition[1], 0.4, self.numParts)
+        ])
+
+    def set_config_params(self, params):
+        self.config_params.update(params)
+    
+    def configure_yaml(self,backend_vocs):
+        # print("backend vocs")
+        # print(backend_vocs)
+        
+        #define the xopt configuration
+        YAML = """
+        xopt:
+            dump_file: dump.yaml
+        generator:
+            name:
+        evaluator:
+            function: __main__.geecs_measurement            
+        vocs:
+            variables:
+                {}
+            objectives: {f: "MAXIMIZE"}
+        sams_thing: 5
+
+        """
+
+        self.yaml_config = yaml.safe_load(YAML)
+        
+        self.yaml_config['evaluator']['function_kwargs'] = {'normalize': self.config_params['normalize'],'shots_per_step': self.config_params['shots_per_step']}
+                
+        for tag in backend_vocs.keys():
+            print(tag)
+            self.yaml_config['vocs']['variables'][tag]=backend_vocs[tag]
+            print(self.yaml_config['vocs']['variables'][tag])
+    
+        if self.config_params['normalize']:
+            for tag in backend_vocs.keys():
+                self.yaml_config['vocs']['variables'][tag]=[-1.0,1.0]
+            keys = self.yaml_config['vocs']['variables'].keys()
+            
+        # New dictionary for initial points
+        init_point = {}
+
+        # Calculate the average of the bounds and assign it to the corresponding key for initial point
+        for key, bounds in backend_vocs.items():
+            average = sum(bounds) / len(bounds)
+            init_point[key] = average
+        
+        
+        # keys_to_add = {
+        #     "name": "upper_confidence_bound",
+        #     "n_initial": 2,
+        #     "acq": {"beta": 0.1}
+        # }
+        #
+        # for key, value in keys_to_add.items():
+        #     self.yaml_config['generator'][key] = value
+
+            
+        
+        if self.config_params['opt_method'] == 'bayes_UCB':
+            self.yaml_config['generator']['name'] = 'upper_confidence_bound'
+            self.yaml_config['generator']['n_initial'] = 2
+            self.yaml_config['generator']['acq'] = {'beta':0.1}
+            self.yaml_config['xopt']['dump_file'] = 'bayes.yaml'
+
+
+        elif self.config_params['opt_method'] == 'nelder':
+            self.yaml_config['generator']['name'] = 'neldermead'
+            self.yaml_config['generator']['adaptive'] = True
+            self.yaml_config['generator']['xatol'] = 0.1
+            self.yaml_config['generator']['fatol'] = 0.05
+            self.yaml_config['generator']['initial_point'] = init_point
+            self.yaml_config['xopt']['dump_file'] = 'nelder.yaml'
+   
+   
+            
+        if self.yaml_config['generator']['name']=='neldermead':
+            if self.config_params['normalize']:
+                initial_point = self.yaml_config['generator']['initial_point']
+
+                normalized_initial_point = {}
+                for key in keys:
+                    normalized_initial_point[key] = geecs_interface.normalize_controls(key, initial_point[key])
+
+                self.yaml_config['generator']['initial_point'] = normalized_initial_point
+                
+        print(self.yaml_config)
+        return self.yaml_config
+        
+    def initialize_xopt(self):
+        print(self.X)
+        self.X = Xopt(config=self.yaml_config)
+        print(self.X)
+            
+        if self.config_params['opt_method'] == 'bayes':
+            # print initial number of points to be generated
+            print(self.X.generator.options.n_initial)
+
+            # call X.step() to generate + evaluate initial points
+            self.X.step()
+
+            # inspect the gathered data
+            self.X.data
+            
+    def xopt_step(self):
+        try:
+            # Attempt to execute code that might raise an exception
+            self.X.step()
+            df = self.X.data
+            #print(df)
+            return df
+        except Exception as e:
+            # Handle exceptions
+            print(f"An error occurred during optimization step: {e}")
+            # Optionally, return a default value or re-raise the exception
+            return None  # or raise e
+
+        
+        
+
+    
+
+    
+    
---
+++ xopt/xopt_tkinter/optimization_tab.py
@@ -0,0 +1,179 @@
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import threading
+
+
+class OptimizationTab(tk.Frame):
+    def __init__(self, master=None, backend=None, geecs_interface=None):
+        super().__init__(master)
+        self.master = master
+        self.backend = backend
+        self.geecs_interface=geecs_interface
+        self.abort_flag = False
+        self.resume_flag = False
+        
+    
+        # Create a frame for the control buttons, which will be positioned at the bottom
+        self.control_frame = tk.Frame(self)
+        self.control_frame.grid(row=2, column=0, columnspan=2, sticky="ew", padx=10, pady=10) # use grid
+
+        # Create the "Run Optimization" button inside the control frame
+        self.run_optimization_button = tk.Button(self.control_frame, text="Run Optimization", command=self.run_optimization)
+        self.run_optimization_button.grid(row=0, column=0, padx=10, pady=10) # changed to grid from pack
+        
+        self.abort_button = tk.Button(self.control_frame, text="Abort Optimization", command=self.abort_optimization)
+        self.abort_button.grid(row=0, column=1, padx=10, pady=10)
+        
+        self.resume_button = tk.Button(self.control_frame, text="Resume/Continue Optimization", command=self.resume_optimization)
+        self.resume_button.grid(row=0, column=2, padx=10, pady=10)
+        
+        self.reset_button = tk.Button(self.control_frame, text="Reset Optimization", command=self.reset_optimization)
+        self.reset_button.grid(row=0, column=3, padx=10, pady=10)
+        
+
+        # Create a container frame for the first plot
+        self.plot_container1 = tk.Frame(self, width=500, height=400, bg="white")  # Set the desired width and height
+        self.plot_container1.grid_propagate(False)  # Prevent the container from resizing to fit its contents
+        self.plot_container1.grid(row=0, column=0, padx=10, pady=10)  # changed to grid
+
+        # Create another container for the second plot
+        self.plot_container2 = tk.Frame(self, width=500, height=400, bg="white")  # Set the desired width and height
+        self.plot_container2.grid_propagate(False)  # Prevent the container from resizing to fit its contents
+        self.plot_container2.grid(row=0, column=1, padx=10, pady=10)  # changed to grid
+
+        # Create the first figure with the desired size
+        self.fig1 = plt.Figure(figsize=(4, 4), dpi=100)
+        self.ax1 = self.fig1.add_subplot(111)
+        self.ax1.set_xlabel("Iteration Number")
+        self.ax1.set_ylabel("Target Function")
+
+        # Add the canvas to the first plot container
+        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.plot_container1)
+        self.canvas1.get_tk_widget().place(relwidth=1, relheight=1)  # Make the canvas fill the plot container
+
+        # Create the second figure with the desired size
+        self.fig2 = plt.Figure(figsize=(4, 4), dpi=100)
+        self.ax2 = self.fig2.add_subplot(111)
+        self.ax2.set_xlabel("Iteration Number")
+        self.ax2.set_ylabel("Control Parameters")
+
+        # Add the canvas to the second plot container
+        self.canvas2 = FigureCanvasTkAgg(self.fig2, master=self.plot_container2)
+        self.canvas2.get_tk_widget().place(relwidth=1, relheight=1)  # Make the canvas fill the plot container
+
+
+
+    def create_widgets(self):
+        # Create a matplotlib figure
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot(111)  # Create one subplot within the figure
+
+        # Embed the figure into the Tkinter window using a canvas
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)  # A tk.DrawingArea.
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        # Add a button to start the optimization process
+        self.optimize_button = tk.Button(self, text="Optimize", command=self.run_optimization)
+        self.optimize_button.pack()
+        
+        
+    def run_optimization(self):
+        # Start the optimization in a new thread
+        self.optimization_thread = threading.Thread(target=self.optimization_process)
+        self.optimization_thread.start()
+    
+
+    def optimization_process(self):
+        try:
+            # Safely get configuration parameters with default values if they don't exist
+            config_params = self.backend.config_params
+            normalize = config_params.get('normalize', False)  # Default to False if not present
+            opt_method = config_params.get('opt_method', '')  # Default to empty string if not present
+            print('opt_method:', opt_method)
+            print('normalize:', normalize)
+        
+            yaml_config = self.backend.configure_yaml(self.geecs_interface.backend_vocs)
+            
+            if self.resume_flag:
+                print("continuing existing optimization")
+            else:
+                self.backend.initialize_xopt()
+        
+            for i in range(self.backend.config_params['opt_steps']):  
+                if self.abort_flag:
+                       print("Optimization aborted by user.")
+                       self.abort_flag = False  # Reset it for the next time
+                       break
+                print('optimization step: ', i)
+                result = self.backend.xopt_step()
+
+                # Check if result is None or empty, and if so, skip to the next iteration
+                if result is None or result.empty:
+                    print("No results returned from the optimization step.")
+                    continue
+
+                # Check if 'f' column exists in the DataFrame
+                if 'f' not in result.columns:
+                    raise ValueError("Result data does not contain 'f' column.")
+
+                # Extract 'f' values
+                y1 = result['f'].values
+
+                # Update the first plot
+                self.ax1.clear()
+                self.ax1.plot(y1)
+                self.ax1.set_title("Optimization Progress")
+                self.ax1.set_xlabel("Iteration Number")
+                self.ax1.set_ylabel("Target Function")
+
+                # Prepare for the second plot
+                self.ax2.clear()
+
+                # Safely get control names, if they don't exist, raise an error
+                if 'vocs' in yaml_config and 'variables' in yaml_config['vocs']:
+                    control_names = yaml_config['vocs']['variables'].keys()
+                else:
+                    raise ValueError("'vocs' or 'variables' not found in yaml_config.")
+
+                # Loop through each control and plot it, if it exists in the result DataFrame
+                for control in control_names:
+                    if control in result.columns:
+                        y2 = result[control].values
+                        self.ax2.plot(y2, label=control)
+                    else:
+                        print(f"Warning: Control '{control}' not found in result data.")
+
+                self.ax2.set_title("Control Parameters Evolution")
+                self.ax2.set_xlabel("Iteration Number")
+                self.ax2.set_ylabel("Control Parameter Value")
+                self.ax2.legend()
+
+                self.canvas1.draw()
+                self.canvas2.draw()
+
+                # Update the GUI
+                self.update()
+
+        except Exception as e:
+            # Log the exception and provide feedback
+            print(f"An error occurred during the optimization process: {e}")
+            # Here, you could also log the exception to a logging service, or show a message box to the user, etc.
+            
+    def abort_optimization(self):
+        self.abort_flag = True
+        print('aborted')
+        # Optionally, you can join the thread here to ensure it's finished before doing anything else
+        # self.optimization_thread.join()
+        
+    def resume_optimization(self):
+        self.resume_flag = True
+        self.run_optimization()
+        print('resuming')
+        
+    def reset_optimization(self):
+        self.resume_flag = False
+        print('resetting')
+
