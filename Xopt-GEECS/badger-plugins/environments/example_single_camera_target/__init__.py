96cb50fa72f233c55b180f2dca666dd4173d36f4
Christopher Doss <CEDoss@lbl.gov>
2024-02-07 17:09:13 -0800

    Example using a single target
    An implementation of the parent's target function for a single target

---
+++ Xopt-GEECS/badger-plugins/environments/example_single_camera_target/__init__.py
@@ -0,0 +1,17 @@
+from ..parent_geecs_env import Environment as geecs_Environment
+
+
+class Environment(geecs_Environment):
+    name = 'example_single_camera_target'
+    variables = {
+        'UC_ChicaneSlit:exposure': [0.01, .9]
+    }
+    observables = ['UC_ChicaneSlit:meancounts']
+    target_meancounts: float = 1000.0
+
+    def get_observables(self, observable_names):
+        return super().get_observables_geecs(observable_names,
+                                             target_function=self.custom_multi_target)
+
+    def custom_multi_target(self, acquired_data):
+        return super().return_multi_target(acquired_data, self.target_meancounts, self.observables)
