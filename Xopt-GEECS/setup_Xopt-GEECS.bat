731f336fd4287b15b1fb15b72f1f7a90a67c19ae
Christopher Doss <CEDoss@lbl.gov>
2024-02-07 17:03:34 -0800

    setup_Xopt-GEECS.bat now auto-generates symlinks
    Just need to specify the conda env directory, and this batch script calls a python script to generate all the symlinks.

--- Xopt-GEECS/setup_Xopt-GEECS.bat
+++ Xopt-GEECS/setup_Xopt-GEECS.bat
@@ -1,13 +1,9 @@
-SET "ANACONDA_Xopt_GEECS=C:\Users\loasis.LOASIS\AppData\Local\anaconda3\envs\Xopt-GEECS"
-:: SET "GEECS_Plugins_Directory=C:\GEECS\Developers Version\source\GEECS-Plugins"
-SET "GEECS_Plugins_Directory=C:\Users\loasis.LOASIS\Documents\GitHub\GEECS-Plugins"
+setlocal
 
-git clone https://github.com/slaclab/Badger-Plugins.git "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins"
+SET ANACONDA_Xopt_GEECS="C:\Users\loasis.LOASIS\AppData\Local\anaconda3\envs\Xopt-GEECS"
+SET PYTHON_SCRIPT=generate_symlink_bat.py
 
-mklink %ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\environments\geecs "%GEECS_Plugins_Directory%\Xopt-GEECS\badger-plugins\environments\geecs"
-mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\environments\camera_exposure_time_test" "%GEECS_Plugins_Directory%\Xopt-GEECS\badger-plugins\environments\camera_exposure_time_test"
-mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\environments\double_camera_exposure_time_test" "%GEECS_Plugins_Directory%\Xopt-GEECS\badger-plugins\environments\double_camera_exposure_time_test"
-mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\environments\parent_geecs_env" "%GEECS_Plugins_Directory%\Xopt-GEECS\badger-plugins\environments\parent_geecs_env"
-mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\environments\HTU_hex_alignment_sim" "%GEECS_Plugins_Directory%\Xopt-GEECS\badger-plugins\environments\HTU_hex_alignment_sim"
-mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\interfaces\geecs" "%GEECS_Plugins_Directory%\Xopt-GEECS\badger-plugins\interfaces\geecs"
-pause
+CALL python "%python_script%" %ANACONDA_Xopt_GEECS%
+CALL autosetup_Xopt-GEECS.bat
+
+endlocal
