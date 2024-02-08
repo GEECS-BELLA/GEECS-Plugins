e7b6c5a8edb9425638510578e37a0ac8fefe04fd
Christopher Doss <CEDoss@lbl.gov>
2024-02-07 17:08:45 -0800

    Renamed these working tests to examples
    camera_exposure_time_test renamed to example_singla_camera_median
    double_camera_experouse_time_test renamed to example_two_cmaera_math

--- Xopt-GEECS/badger-plugins/environments/example_single_camera_median/__init__.py
+++ Xopt-GEECS/badger-plugins/environments/example_single_camera_median/__init__.py
@@ -2,7 +2,7 @@ from ..parent_geecs_env import Environment as geecs_Environment
 
 
 class Environment(geecs_Environment):
-    name = 'camera_exposure_time_test'
+    name = 'example_single_camera_median'
     variables = {
         'UC_ChicaneSlit:exposure': [0.01, .9],
         'UC_ChicaneSlit:triggerdelay': [300.0, 600.0],
--- Xopt-GEECS/badger-plugins/environments/example_two_camera_math/__init__.py
+++ Xopt-GEECS/badger-plugins/environments/example_two_camera_math/__init__.py
@@ -1,9 +1,10 @@
 import numpy as np
+from typing import List
 from ..parent_geecs_env import Environment as geecs_Environment
 
 
 class Environment(geecs_Environment):
-    name = 'double_camera_exposure_time_test'
+    name = 'example_two_camera_math'
 
     variables = {
         'UC_ChicaneSlit:exposure': [0.01, .9],
@@ -14,22 +15,25 @@ class Environment(geecs_Environment):
 
     observables = ['Target Function']
 
-    some_parameter: str = 'test'
-    target_meancounts: float = 500
+    target_sumcounts: float = 500
+    target_function_inputs: List[str] = ['UC_ChicaneSlit:meancounts',
+                                         'UC_DiagnosticsPhosphor:meancounts']
 
     def get_observables(self, observable_names):
-        target_function_inputs = ['UC_ChicaneSlit:meancounts',
-                                  'UC_DiagnosticsPhosphor:meancounts']
         return super().get_observables_geecs(observable_names,
-                                             device_acquisitions=target_function_inputs,
+                                             device_acquisitions=self.target_function_inputs,
                                              target_function=self.sum_camera_counts)
 
     def sum_camera_counts(self, acquired_data):
-        median_vals = {key: np.median(values) for key, values in acquired_data.items()}
+        median_vals = super().return_median_value(acquired_data)
         print("Median Vals: ", median_vals)
 
-        summed_counts = np.sum(list(median_vals.values()))
-        target = 100*np.exp(-np.square(summed_counts - self.target_meancounts)/np.square(0.2*self.target_meancounts))
+        val1 = median_vals.get(self.target_function_inputs[0])
+        val2 = median_vals.get(self.target_function_inputs[1])
+        summed_counts = val1+val2
+        print("Summed Counts: ", summed_counts)
+
+        target = np.exp(-np.square(summed_counts - self.target_sumcounts)/np.square(0.2*self.target_sumcounts))
         target_return = {self.observables[0]: target}
         print("Target Ret.: ", target_return)
         return target_return
