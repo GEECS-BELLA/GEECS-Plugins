fc2add483b546d8865a4215317b6a7030caa9844
Christopher Doss <CEDoss@lbl.gov>
2024-02-09 09:57:23 -0800

    Symlink script can now only link chosen exp.
    Reads in the config file and based on the current experiment will only link environments within the respective subfolder in environments.  Also included flags on whether or not to link the example environments (default true) and the old legacy environments (default true for now, false once we are done using them)

--- Xopt-GEECS/generate_symlink_bat.py
+++ Xopt-GEECS/generate_symlink_bat.py
@@ -1,5 +1,5 @@
 """
-Writes a .bat file to symlink all of the folders within badger-plugins to the conda virtual environment location for
+Writes a .bat file to symlink all the folders within badger-plugins to the conda virtual environment location for
 badger.  Make sure that your conda environment is specified in setup_Xopt-GEECS.bat
 (the default is 'C:/Users/loasis.LOASIS/AppData/Local/anaconda3/env/Xopt-GEECS')
 
@@ -8,13 +8,33 @@ badger.  Make sure that your conda environment is specified in setup_Xopt-GEECS.
 
 import os
 import sys
-from geecs_python_api.controls.interface import load_config
+import configparser
+
+
+default_conda_env_directory = r'C:\Users\loasis.LOASIS\AppData\Local\anaconda3\envs\Xopt-GEECS'
+environment_experiment_dictionary = {
+    'Undulator': 'undulator',
+    'Bella': 'pw'
+}
+link_example_envs = True
+link_legacy_envs = True
+
+
+def load_config():
+    config_file = configparser.ConfigParser()
+    config_path = os.path.expanduser('~/.config/geecs_python_api/config.ini')
+    if os.path.exists(config_path):
+        config_file.read(config_path)
+        return config_file
+    else:
+        return None
+
 
 if len(sys.argv) > 1:
     conda_env_directory = sys.argv[1]
 else:
-    print("No conda directory given.  Assuming default in py script:")
-    conda_env_directory = r'C:\Users\loasis.LOASIS\AppData\Local\anaconda3\envs\Xopt-GEECS'
+    print("No conda directory given.  Assuming the default in .py script:")
+    conda_env_directory = default_conda_env_directory
 
 config = load_config()
 if config and 'Experiment' in config and 'expt' in config['Experiment']:
@@ -25,14 +45,12 @@ else:
           "be a problem for you.")
     default_experiment = 'Undulator'
 
-environment_experiment_dictionary = {
-    'Undulator': 'undulator',
-    'WhatIsThePWExperimentName?': 'pw'
-}
-environment_subfolder_list = ['examples',
-                              'geecs_general',
-                              'legacy',
+environment_subfolder_list = ['geecs_general',
                               environment_experiment_dictionary[default_experiment]]
+if link_example_envs:
+    environment_subfolder_list.append('examples')
+if link_legacy_envs:
+    environment_subfolder_list.append('legacy')
 
 environment_folder = r'\badger-plugins\environments'
 current_directory = os.getcwd()
@@ -41,8 +59,10 @@ batch_filename = current_directory + r'\autosetup_Xopt-GEECS.bat'
 with open(batch_filename, 'w') as file:
     file.write(r'SET "ANACONDA_Xopt_GEECS=' + conda_env_directory + '"\n')
     file.write(r'SET "Xopt_GEECS_Directory=' + str(current_directory) + '"\n\n')
-    file.write(r'git clone https://github.com/slaclab/Badger-Plugins.git "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins"'+'\n\n')
-    file.write(r'mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\interfaces\geecs" "%Xopt_GEECS_Directory%\badger-plugins\interfaces\geecs"'+'\n\n')
+    file.write(r'git clone https://github.com/slaclab/Badger-Plugins.git "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins"' +
+               '\n\n')
+    file.write(r'mklink "%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\interfaces\geecs" '
+               r'"%Xopt_GEECS_Directory%\badger-plugins\interfaces\geecs"'+'\n\n')
 
     print("Writing links to the following environments:")
     for sub_experiment in environment_subfolder_list:
@@ -56,7 +76,8 @@ with open(batch_filename, 'w') as file:
         conda_env = r'%ANACONDA_Xopt_GEECS%\Lib\Badger-Plugins\environments'
         for environment in environments:
             env_folder = '\\' + str(environment)
-            file.write(r'mklink "' + conda_env + env_folder + '" "' + badger_env_dir + '\\' + sub_experiment + env_folder + '"\n')
+            file.write(r'mklink "' + conda_env + env_folder + '" "' +
+                       badger_env_dir + '\\' + sub_experiment + env_folder + '"\n')
 
     file.write('\n' + r'ECHO Finished symlinks, deleting auto-script...' + '\n')
     file.write(r'pause' + '\n')
