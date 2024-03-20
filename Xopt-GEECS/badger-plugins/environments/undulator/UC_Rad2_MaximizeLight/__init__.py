7b9cc85ea743e6a0b22e135eaa2d9f055b87842a
Christopher Doss <CEDoss@lbl.gov>
2024-03-20 15:41:57 -0700

    Minor code/comment block cleanup

--- Xopt-GEECS/badger-plugins/environments/undulator/UC_Rad2_MaximizeLight/__init__.py
+++ Xopt-GEECS/badger-plugins/environments/undulator/UC_Rad2_MaximizeLight/__init__.py
@@ -1,5 +1,3 @@
-import numpy as np
-from typing import List
 try:  # The top import works in Badger with symlinks, the bottom import lets PyCharm know what is being imported
     from ..base_geecs_env import Environment as geecs_Environment
 except ImportError:
@@ -18,25 +16,3 @@ class Environment(geecs_Environment):
     }
 
     observables = ['UC_UndulatorRad2:Python Result 6']  # Optimization Factor
-
-    """
-    observables = ['Target Function']
-    
-    target_function_inputs: List[str] = ['UC_HiResMagCam:Python Result 14']
-
-    def get_observables(self, observable_names):
-        return super().get_observables_geecs(observable_names,
-                                             device_acquisitions=self.target_function_inputs,
-                                             target_function=self.custom_function)
-
-    def custom_function(self, acquired_data):
-        median_vals = super().return_median_value(acquired_data)
-        print("Median Vals: ", median_vals)
-
-        val1 = median_vals.get(self.target_function_inputs[0])
-
-        target = (np.exp(val1)-1)/90
-        target_return = {self.observables[0]: target}
-        print("Target Ret.: ", target_return)
-        return target_return
-    """
