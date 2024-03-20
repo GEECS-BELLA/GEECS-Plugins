f5ca7cf597b02cea5b81e8e2a78e14b0cb3eb4d5
Christopher Doss <CEDoss@lbl.gov>
2024-03-20 15:42:50 -0700

    New Optimization Function for HiResMagCam
    Didn't work perfectly, but it worked slightly better than the built-in one in the analyzer.  This template at least will be helpful for playing around with the target function more.

--- Xopt-GEECS/badger-plugins/environments/undulator/HiResMagCam_100MeV/__init__.py
+++ Xopt-GEECS/badger-plugins/environments/undulator/HiResMagCam_100MeV/__init__.py
@@ -10,8 +10,8 @@ class Environment(geecs_Environment):
     name = 'HiResMagCam_100MeV'
     variables = {
         # 'U_ESP_JetXYZ:Position.Axis 1': [6.3, 6.7],  # Jet_X (mm)
-        'U_ESP_JetXYZ:Position.Axis 2': [-7.6, -7.0],  # Jet_Y (mm)
-        'U_ESP_JetXYZ:Position.Axis 3': [0.1, 5.0],  # Jet_Z (mm)
+        # 'U_ESP_JetXYZ:Position.Axis 2': [-7.6, -7.0],  # Jet_Y (mm)
+        'U_ESP_JetXYZ:Position.Axis 3': [2.0, 6.0],  # Jet_Z (mm)
         'U_HP_Daq:AnalogOutput.Channel 1': [1.5, 3.0],  # PressureControlVoltage
         'U_ModeImagerESP:Position.Axis 2': [-17.25, -17.10],  # JetBlade
     }
@@ -19,7 +19,19 @@ class Environment(geecs_Environment):
     # observables = ['UC_HiResMagCam:Python Result 14']  # Optimization Factor
 
     observables = ['Target Function']
-    
+
+    target_energy: float = 100.0
+    target_energy_sigma: float = 10.0
+    target_energy_weight: int = 1
+
+    target_charge: float = 100.0
+    target_charge_sigma: float = 40.0
+    target_charge_weight: int = 1
+
+    target_fwhm: float = 0.0
+    target_fwhm_sigma: float = 10.0
+    target_fwhm_weight: int = 1
+
     target_function_inputs: List[str] = ['UC_HiResMagCam:Python Result 5',   # Peak Energy (MeV)
                                          'UC_HiResMagCam:Python Result 3',   # Charge on Camera (pC)
                                          'UC_HiResMagCam:Python Result 15',  # FWHM (MeV)
@@ -31,14 +43,20 @@ class Environment(geecs_Environment):
                                              target_function=self.hiresmagspec_quality)
 
     def hiresmagspec_quality(self, acquired_data):
-        peak_energy = acquired_data.get(self.target_function_inputs[0])
-        beam_charge = acquired_data.get(self.target_function_inputs[1])
-        fwhm_energy = acquired_data.get(self.target_function_inputs[2])
+        peak_energy = np.array(acquired_data.get(self.target_function_inputs[0]))
+        beam_charge = np.array(acquired_data.get(self.target_function_inputs[1]))
+        fwhm_energy = np.array(acquired_data.get(self.target_function_inputs[2]))
 
-        summed_counts = val1 + val2
-        print("Summed Counts: ", summed_counts)
+        peak_energy_target = np.exp(-np.square(peak_energy - self.target_energy) / np.square(self.target_energy_sigma))
+        beam_charge_target = np.exp(-np.square(beam_charge - self.target_charge) / np.square(self.target_charge_sigma))
+        fwhm_energy_target = np.exp(-np.square(fwhm_energy - self.target_fwhm) / np.square(self.target_fwhm_sigma))
 
-        target = np.exp(-np.square(summed_counts - self.target_sumcounts) / np.square(0.2 * self.target_sumcounts))
+        print("Energy, Charge, FWHM Individual Targets:")
+        print(peak_energy_target, beam_charge_target, fwhm_energy_target)
+
+        target = peak_energy_target * beam_charge_target * fwhm_energy_target
+        print("Target: ", target, "(Maximum:", self.target_energy_weight*self.target_fwhm_weight*self.target_energy_weight,")")
+
         target_return = {self.observables[0]: target}
         print("Target Ret.: ", target_return)
         return target_return
