359b07b9c647facf768ec8ace7369593808880d0
Christopher Doss <CEDoss@lbl.gov>
2024-03-22 16:42:39 -0700

    Some carryover edits for the ALine3 optimizer
    Probably should have committed before this branch, oh well :(

--- Xopt-GEECS/badger-plugins/environments/undulator/EBeamFocusOptimizer/__init__.py
+++ Xopt-GEECS/badger-plugins/environments/undulator/EBeamFocusOptimizer/__init__.py
@@ -9,12 +9,17 @@ except ImportError:
 class Environment(geecs_Environment):
     name = 'EBeamFocusOptimizer'
     variables = {
-        'U_EMQTripletBipolar:Current_Limit.Ch1': [0.5, 1.0],
-        'U_EMQTripletBipolar:Current_Limit.Ch2': [-1.2, -0.4],
-        'U_EMQTripletBipolar:Current_Limit.Ch3': [0.5, 1.0]
+        'U_EMQTripletBipolar:Current_Limit.Ch1': [0.5, 1.3],
+        'U_EMQTripletBipolar:Current_Limit.Ch2': [-1.5, -0.4],
+        'U_EMQTripletBipolar:Current_Limit.Ch3': [0.7, 2.0]
     }
-
-    camera_name: str = 'UC_AlineEBeam3'
+    """
+    variables = {
+        'U_EMQTripletBipolar:Current_Limit.Ch1': [0.5, 2.0],
+        'U_EMQTripletBipolar:Current_Limit.Ch2': [-1.5, -0.4]
+    }
+    """
+    camera_name: str = 'UC_ALineEBeam3'
     power: int = 1
     # observables = ['UC_ALineEBeam3:MaxCounts']
     observables = ['Target Function']
@@ -22,6 +27,8 @@ class Environment(geecs_Environment):
     target_function_inputs: List[str] = [camera_name+':MaxCounts',
                                          camera_name+':FWHMx',
                                          camera_name+':FWHMy',
+                                         camera_name+':Centroidx',
+                                         camera_name+':Centroidy',
                                          ]
 
     def get_observables(self, observable_names):
@@ -33,8 +40,14 @@ class Environment(geecs_Environment):
         maxcounts = np.array(acquired_data.get(self.target_function_inputs[0]))
         fwhmx = np.array(acquired_data.get(self.target_function_inputs[1]))
         fwhmy = np.array(acquired_data.get(self.target_function_inputs[2]))
+        centx = np.array(acquired_data.get(self.target_function_inputs[3]))
+        centy = np.array(acquired_data.get(self.target_function_inputs[4]))
 
         target = maxcounts / (1 + np.power(np.abs(fwhmx - fwhmy), self.power))
+        for i in range(len(maxcounts)):
+            if centx[i] < 100 or centy[i] > 600:
+                target[i] = 0
+
         target_return = {self.observables[0]: target}
         print("Target Ret.: ", target_return)
         return target_return
