ccac8e3f4d9bd78227a4132cef5b118d3241f1c0
Christopher Doss <CEDoss@lbl.gov>
2024-03-22 16:45:52 -0700

    New automation capabilities for installation
    Auto-generates the config file and creates a shortcut if neither exists
    Moved much of the hard-coded variables and paths to the config file
    Symlink script now also deletes all old symlinks in that virtual environment folder
    Functionality to look for and create new experimental subfolders apart from "undulator" and "pw"
    More print and echo statements so that it is clear what the script is doing

--- Xopt-GEECS/setup_Xopt-GEECS.bat
+++ Xopt-GEECS/setup_Xopt-GEECS.bat
@@ -1,9 +1,13 @@
+@echo off
 setlocal
 
-SET ANACONDA_Xopt_GEECS="C:\Users\loasis.LOASIS\AppData\Local\anaconda3\envs\Xopt-GEECS"
 SET PYTHON_SCRIPT=generate_symlink_bat.py
 
 CALL python "%python_script%" %ANACONDA_Xopt_GEECS%
-CALL autosetup_Xopt-GEECS.bat
+if exist "autosetup_Xopt-GEECS.bat" (
+    CALL autosetup_Xopt-GEECS.bat
+    ECHO.
+    ECHO Run Badger on a conda terminal using 'badger -g' after activating the Xopt-GEECS environment
+)
 
 endlocal
