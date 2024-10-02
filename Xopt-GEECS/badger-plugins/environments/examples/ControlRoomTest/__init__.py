330b87ff414192ff781520291295a44ce0b4fd00
chetanyajain-lbl <167905726+chetanyajain-lbl@users.noreply.github.com>
2024-10-02 13:13:52 -0700

    My Changes
    My Changes

---
+++ Xopt-GEECS/badger-plugins/environments/examples/ControlRoomTest/__init__.py
@@ -0,0 +1,20 @@
+try:  # The top import works in Badger with symlinks, the bottom import lets PyCharm know what is being imported
+    from ..base_geecs_env import Environment as geecs_Environment
+except ImportError:
+    from ...geecs_general.base_geecs_env import Environment as geecs_Environment
+
+
+class Environment(geecs_Environment):
+    name = 'example_single_camera_target'
+    variables = {
+        'CAM-CR-Beampointing2:exposure': [0.01, .2]
+    }
+    observables = ['CAM-CR-Beampointing2:meancounts']
+    target_meancounts: float = 100.0
+
+    def get_observables(self, observable_names):
+        return super().get_observables_geecs(observable_names,
+                                             target_function=self.custom_multi_target)
+
+    def custom_multi_target(self, acquired_data):
+        return super().return_multi_target(acquired_data, self.target_meancounts, self.observables)
