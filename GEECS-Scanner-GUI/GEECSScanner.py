b2ded9807e023894434ef291c548d21d58e3efc1
Christopher Doss <CEDoss@lbl.gov>
2024-10-07 16:02:43 -0700

    Initial work on the GEECSScanner GUI
    Finished some basic test functionality for file searching and button selections

---
+++ GEECS-Scanner-GUI/GEECSScanner.py
@@ -0,0 +1,122 @@
+"""
+Script to contain the logic for the GEECSScanner GUI
+
+-Chris
+"""
+
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QCompleter
+from PyQt5.QtCore import Qt, QEvent
+from GEECSScanner_ui import Ui_MainWindow
+from geecs_python_api.controls.interface import load_config
+
+
+class GEECSScannerWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        config = load_config()
+        default_experiment = config['Experiment']['expt']
+        if os.path.isdir("./experiments/" + default_experiment):
+            self.experiment = default_experiment
+        else:
+            self.experiment = "<None Selected>"
+
+        self.ui.experimentDisplay.setText(self.experiment)
+        self.ui.experimentDisplay.setReadOnly(True)
+        self.ui.experimentDisplay.installEventFilter(self)
+
+        self.populate_found_list()
+
+        # Connect buttons to their respective functions
+        self.ui.addDeviceButton.clicked.connect(self.add_files)
+        self.ui.foundDevices.itemDoubleClicked.connect(self.add_files)
+        self.ui.removeDeviceButton.clicked.connect(self.remove_files)
+        self.ui.selectedDevices.itemDoubleClicked.connect(self.remove_files)
+
+        self.ui.noscanRadioButton.setChecked(True)
+        self.ui.noscanRadioButton.toggled.connect(self.update_scan_edit_state)
+        self.ui.scanRadioButton.toggled.connect(self.update_scan_edit_state)
+
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.MouseButtonPress and source == self.ui.experimentDisplay:
+            self.show_experiment_list()
+            return True
+        return super().eventFilter(source, event)
+
+    def show_experiment_list(self):
+        folders = [f for f in os.listdir("./experiments/") if os.path.isdir(os.path.join("./experiments", f))]
+        completer = QCompleter(folders, self)
+        completer.setCompletionMode(QCompleter.PopupCompletion)
+        completer.setCaseSensitivity(Qt.CaseInsensitive)
+
+        self.ui.experimentDisplay.setCompleter(completer)
+        self.ui.experimentDisplay.setReadOnly(False)
+        self.ui.experimentDisplay.clear()
+        self.ui.experimentDisplay.setFocus()
+        completer.complete()
+        self.ui.experimentDisplay.returnPressed.connect(self.experiment_selected)
+
+    def experiment_selected(self):
+        self.clear_lists()
+        selected_experiment = self.ui.experimentDisplay.text()
+        new_folder_path = os.path.join("./experiments/", selected_experiment)
+        if os.path.isdir(new_folder_path):
+            self.experiment = selected_experiment
+            self.ui.experimentDisplay.setText(self.experiment)
+            self.populate_found_list()
+
+        self.ui.experimentDisplay.setReadOnly(True)
+
+    def clear_lists(self):
+        self.ui.selectedDevices.clear()
+        self.ui.foundDevices.clear()
+
+    def populate_found_list(self):
+        # List all files in the save_devices folder under chosen experiment:
+        experiment_folder = "./experiments/" + self.experiment + "/save_devices/"
+        for file_name in os.listdir(experiment_folder):
+            full_path = os.path.join(experiment_folder, file_name)
+            if os.path.isfile(full_path):
+                self.ui.foundDevices.addItem(file_name)
+
+    def add_files(self):
+        # Move selected files from the "Found" list to the "Selected" list
+        selected_items = self.ui.foundDevices.selectedItems()
+        for item in selected_items:
+            self.ui.foundDevices.takeItem(self.ui.foundDevices.row(item))
+            self.ui.selectedDevices.addItem(item)
+
+    def remove_files(self):
+        # Move selected files from the "Selected" list back to the "Found" list
+        selected_items = self.ui.selectedDevices.selectedItems()
+        for item in selected_items:
+            self.ui.selectedDevices.takeItem(self.ui.selectedDevices.row(item))
+            self.ui.foundDevices.addItem(item)
+
+    def update_scan_edit_state(self):
+        if self.ui.noscanRadioButton.isChecked():
+            self.ui.lineScanVariable.setEnabled(False)
+            self.ui.lineStartValue.setEnabled(False)
+            self.ui.lineStopValue.setEnabled(False)
+            self.ui.lineStepSize.setEnabled(False)
+            self.ui.lineNumShots.setEnabled(True)
+
+        else:
+            self.ui.lineScanVariable.setEnabled(True)
+            self.ui.lineStartValue.setEnabled(True)
+            self.ui.lineStopValue.setEnabled(True)
+            self.ui.lineStepSize.setEnabled(True)
+            self.ui.lineNumShots.setEnabled(False)
+
+if __name__ == ('__main__'):
+    app = QApplication(sys.argv)
+
+    file_selector = GEECSScannerWindow()
+    file_selector.show()
+
+    sys.exit(app.exec_())
