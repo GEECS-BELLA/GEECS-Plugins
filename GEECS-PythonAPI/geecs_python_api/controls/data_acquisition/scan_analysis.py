03e6086193e5a349fd0e616a88c7e6986db8a5da
kjensen11 <40208053+kjensen11@users.noreply.github.com>
2024-11-01 10:58:06 -0700

    Update CameraImageAnalysis and VisaEBeamAnalysis
    Generalized parent CameraImageAnalysis class and created child VisaEBeamAnalysis class. Currently, the run_analysis function of VisaEBeamAnalysis is the same as the previous CameraImageAnalysis function. I did not generalize the CameraImageAnalysis run_analysis function.
    
    Changed post_analysis_class in visa 1-8 spectrometer setup yaml files to point to VisaEBeamAnalysis.

--- GEECS-PythonAPI/geecs_python_api/controls/data_acquisition/scan_analysis.py
+++ GEECS-PythonAPI/geecs_python_api/controls/data_acquisition/scan_analysis.py
@@ -16,6 +16,7 @@ import re
 
 from image_analysis.utils import read_imaq_png_image
 
+# %% base scan analysis class
 class ScanAnalysis:
     """
     Base class for performing analysis on scan data. Handles loading auxiliary data and extracting
@@ -30,7 +31,7 @@ class ScanAnalysis:
         auxiliary_data (pd.DataFrame): DataFrame containing auxiliary scan data.
     """
     def __init__(self, scan_directory, use_gui = True, experiment_dir = "Undulator"):
-    
+
         """
         Initialize the ScanAnalysis class.
 
@@ -43,31 +44,31 @@ class ScanAnalysis:
         self.ini_file_path = self.scan_directory / f"ScanInfo{self.scan_directory.name}.ini"
         self.noscan = False
         self.use_gui = use_gui
-        
-        
+
+
         try:
             # Extract the scan parameter
             self.scan_parameter = self.extract_scan_parameter_from_ini(self.ini_file_path)
-            
+
             logging.info(f"Scan parameter is: {self.scan_parameter}.")
             s_param = self.scan_parameter.lower()
 
             if s_param == 'noscan' or s_param == 'shotnumber':
                 logging.warning("No parameter varied during the scan, setting noscan flag.")
                 self.noscan = True
-            
+
             self.bins, self.auxiliary_data, self.binned_param_values = self.load_auxiliary_data()
-            
+
             if self.auxiliary_data is None:
                 logging.warning("Scan parameter not found in auxiliary data. Possible aborted scan. Skipping analysis.")
                 return  # Stop further execution cleanly
-                
+
             self.total_shots = len(self.auxiliary_data)
-            
+
         except FileNotFoundError as e:
             logging.warining(f"Warning: {e}. Could not find auxiliary or .ini file in {self.scan_directory}. Skipping analysis.")
             return
-        
+
 
 
     def extract_scan_parameter_from_ini(self, ini_file_path):
@@ -101,32 +102,32 @@ class ScanAnalysis:
                 # Find the scan parameter column and calculate the binned values
                 scan_param_column = self.find_scan_param_column(auxiliary_data)[0]
                 binned_param_values = auxiliary_data.groupby('Bin #')[scan_param_column].mean().values
-    
+
                 return bins, auxiliary_data, binned_param_values
 
             else:
                 return bins, auxiliary_data, None
-        
+
         except (KeyError, FileNotFoundError) as e:
             logging.warning(f"Warning: {e}. Scan parameter not found in auxiliary data. Possible aborted scan. Skipping analysis")
             return None, None, None
-            
+
     def close_or_show_plot(self):
         """Decide whether to display or close plots based on the use_gui setting."""
         if not self.use_gui:
             plt.show()  # Display for interactive use
         else:
             plt.close('all')  # Ensure plots close when not using the GUI
-    
+
 
     def generate_limited_shotnumber_labels(self, total_shots, max_labels=20):
         """
         Generate a list of shot number labels with a maximum of `max_labels`.
-    
+
         Args:
             total_shots (int): Total number of shots.
             max_labels (int): Maximum number of labels to display.
-    
+
         Returns:
             np.ndarray: Array of shot numbers, spaced out if necessary.
         """
@@ -137,12 +138,12 @@ class ScanAnalysis:
             # Otherwise, return a spaced-out array with at most max_labels
             step = total_shots // max_labels
             return np.arange(1, total_shots + 1, step)
-        
+
     def find_scan_param_column(self, auxiliary_data):
         """
         Find the column in the auxiliary data corresponding to the scan parameter.
         The method strips unnecessary characters (e.g., quotes) and handles cases where an alias is present.
-    
+
         Returns:
             tuple: A tuple containing the column name and alias (if any) for the scan parameter.
         """
@@ -156,10 +157,11 @@ class ScanAnalysis:
                 if self.scan_parameter in column.split(' Alias:')[0]:
                     # Return the column and the alias if present
                     return column, column.split('Alias:')[-1].strip() if 'Alias:' in column else column
-    
+
             logging.warning(f"Warning: Could not find column containing scan parameter: {self.scan_parameter}")
             return None, None
 
+# %% mag spec analysis classes
 class MagSpecStitcherAnalysis(ScanAnalysis):
     def __init__(self, scan_directory, device_name):
         super().__init__(scan_directory)
@@ -170,7 +172,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
         if not self.data_subdirectory.exists() or not any(self.data_subdirectory.iterdir()):
             logging.warning(f"Warning: Data directory '{self.data_subdirectory}' does not exist or is empty. Skipping analysis.")
             self.data_subdirectory = None
-        
+
     def load_charge_data(self):
         """
         Load charge-per-energy data from files, using shot numbers from auxiliary data.
@@ -179,7 +181,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
         charge_density_matrix = []
         energy_values = None
         missing_shots_placeholder = []  # To keep track of shots missing data
-        
+
         # Get the shot numbers from the auxiliary data
         shot_numbers = self.auxiliary_data['Shotnumber'].values
 
@@ -199,7 +201,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
                     logging.warning(f"Missing data for shot {shot_num}, adding placeholder.")
                     charge_density_matrix.append(None)
                     missing_shots_placeholder.append(len(charge_density_matrix) - 1)  # Track the index of missing shots
-            
+
             except Exception as e:
                 logging.error(f"Error reading data for shot {shot_num}: {e}")
                 charge_density_matrix.append(None)  # Append None in case of error
@@ -215,7 +217,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
             charge_density_matrix[idx] = np.zeros_like(energy_values)
 
         return energy_values, np.array(charge_density_matrix)
-    
+
 
     def interpolate_data(self, energy_values, charge_density_matrix, min_energy=0.06, max_energy=.3, num_points=1500):
         """
@@ -230,7 +232,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
             except Exception as e:
                 logging.warning(f"Interpolation failed for shot {i}. Using zeros. Error: {e}")
                 interpolated_matrix[i] = np.zeros(num_points)  # Handle any interpolation failure
-            
+
             # interpolated_matrix[i] = np.interp(linear_energy_axis, energy_values, row)
 
         return linear_energy_axis, interpolated_matrix
@@ -273,7 +275,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
             save_path = Path(save_dir) / save_name
             plt.savefig(save_path, bbox_inches='tight')
             logging.info(f"Plot saved to {save_path}")
-        
+
     def run_analysis(self):
         """
         Main function to run the analysis and generate plots.
@@ -284,7 +286,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
 
         try:
             energy_values, charge_density_matrix = self.load_charge_data()
-            
+
             if energy_values is None or len(charge_density_matrix) == 0:
                 logging.error("No valid charge data found. Skipping analysis.")
                 return
@@ -319,6 +321,7 @@ class MagSpecStitcherAnalysis(ScanAnalysis):
             logging.warning(f"Warning: Analysis failed due to: {e}")
             return
 
+# %% camera analysis classes
 class CameraImageAnalysis(ScanAnalysis):
 
     def __init__(self, scan_directory, device_name, use_gui=True, experiment_dir = 'Undulator'):
@@ -329,17 +332,17 @@ class CameraImageAnalysis(ScanAnalysis):
             scan_directory (str or Path): Path to the scan directory containing data.
             device_name (str): Name of the device to construct the subdirectory path.
         """
-        super().__init__(scan_directory, use_gui=use_gui)  # Pass use_gui to the parent class        
+        super().__init__(scan_directory, use_gui=use_gui)  # Pass use_gui to the parent class
         # Construct the subdirectory for images based on device name
         self.device_name = device_name
-        self.data_subdirectory = Path(scan_directory) / f"{device_name}"        
+        self.data_subdirectory = Path(scan_directory) / f"{device_name}"
         self.experiment_dir = experiment_dir
-        
+
         self.camera_analysis_config_path = get_full_config_path(self.experiment_dir, 'aux_configs', 'camera_analysis_settings.yaml')
         self.camera_analysis_configs = None
 
-        
-        
+
+
         if self.camera_analysis_config_path.exists():
             self.load_camera_analysis_config()
 
@@ -347,16 +350,16 @@ class CameraImageAnalysis(ScanAnalysis):
         if not self.data_subdirectory.exists() or not any(self.data_subdirectory.iterdir()):
             logging.warning(f"Warning: Data directory '{self.data_subdirectory}' does not exist or is empty. Skipping analysis.")
             self.data_subdirectory = None
-            
+
     def load_camera_analysis_config(self):
-    
+
         """
         Load the master camera configs from the given YAML file.
 
         Returns:
             dict: A dictionary of analysis configs loaded from the YAML file.
         """
-    
+
         camaera_analysis_configs_file = str(self.camera_analysis_config_path)  # Convert Path object to string
         with open(camaera_analysis_configs_file, 'r') as file:
             self.camera_analysis_configs = yaml.safe_load(file)
@@ -431,7 +434,7 @@ class CameraImageAnalysis(ScanAnalysis):
             bit_depth (int): The bit depth of the saved image (default is 16-bit).
         """
         save_path = Path(save_dir) / save_name
-        
+
         # Save in 16-bit for the averaged image
         if bit_depth == 16:
             plt.imsave(save_path, image, cmap='gray', format='png', vmin=0, vmax=65535)  # 16-bit depth
@@ -452,9 +455,9 @@ class CameraImageAnalysis(ScanAnalysis):
         """
         # Normalize 16-bit image to 8-bit by scaling the pixel values
         normalized_image = (image/(2**16-1)*255).astype(np.uint8)
-        
+
         return normalized_image
-       
+
     def create_image_array(self, avg_images):
         """
         Arrange the averaged images into a sensibly sized grid and display them with scan parameter labels.
@@ -477,7 +480,7 @@ class CameraImageAnalysis(ScanAnalysis):
 
         # Flatten axes array for easy indexing (if there's only one row/col, axs won't be a 2D array)
         axs = axs.flatten()
-        
+
         # # Flatten all images in the batch to find global percentiles
         all_pixels = np.concatenate([img.ravel() for img in avg_images if img is not None])
 
@@ -502,8 +505,9 @@ class CameraImageAnalysis(ScanAnalysis):
         plt.savefig(Path(self.scan_directory) / save_name, bbox_inches='tight')
         logging.info(f"Saved final image grid as {save_name}.")
         self.close_or_show_plot()
- 
-    def create_cross_mask(self, image, cross_center, angle, cross_height=54, cross_width=54, thickness=8):
+
+    def create_cross_mask(self, image, cross_center, angle, cross_height=54,
+                          cross_width=54, thickness=8):
         """
         Creates a mask with a cross centered at `cross_center` with the cross being zeros and the rest ones.
 
@@ -538,17 +542,20 @@ class CameraImageAnalysis(ScanAnalysis):
         """
         mask1 = self.create_cross_mask(image, analysis_settings['Cross1'], analysis_settings['Rotate'])
         mask2 = self.create_cross_mask(image, analysis_settings['Cross2'], analysis_settings['Rotate'])
-        
+
         masked_image = image * mask1 * mask2
         cropped_image = masked_image[analysis_settings['Top ROI']:analysis_settings['Top ROI'] + analysis_settings['Size_Y'],
                               analysis_settings['Left ROI']:analysis_settings['Left ROI'] + analysis_settings['Size_X']]
 
         processed_image = cropped_image
         return processed_image
-   
+
     def run_analysis(self):
         """
         Main function to run the image analysis.
+
+        MAY BE DEPRECATED. DEDICATED RUN ANALYSIS FOR VISA IMAGES EXISTS BELOW.
+        SHOULD REDUCE FOR BASE IMAGE ANALYSIS.
         """
         if self.data_subdirectory is None or self.auxiliary_data is None:
             logging.info(f"Skipping analysis due to missing data or auxiliary file.")
@@ -569,20 +576,20 @@ class CameraImageAnalysis(ScanAnalysis):
 
                 # Average the images
                 avg_image = self.average_images(images)
-                
-                
+
+
                 if self.device_name in self.camera_analysis_configs.keys():
                     analysis_settings = self.camera_analysis_configs[self.device_name]
                     avg_image_processed = self.preprocess_visa_image(avg_image, analysis_settings)
                     avg_images.append(avg_image_processed)
-                    
+
                     # Save the averaged image
                     save_name = f'{self.device_name}_{bin_number}_processed.png'
                     self.save_image(avg_image_processed, save_dir=self.scan_directory, save_name=save_name)
-                    
-                else: 
+
+                else:
                     avg_images.append(avg_image)
-                
+
 
                 # Save the averaged image
                 save_name = f'{self.device_name}_{bin_number}.png'
@@ -593,6 +600,124 @@ class CameraImageAnalysis(ScanAnalysis):
             # Once all bins are processed, create an array of the averaged images
             self.create_image_array(avg_images)
 
+        except Exception as e:
+            logging.warning(f"Warning: Image analysis failed due to: {e}")
+            return
+
+class VisaEBeamAnalysis(CameraImageAnalysis):
+
+    def __init__(self, scan_directory, device_name, use_gui=True, experiment_dir = 'Undulator'):
+        """
+        Initialize the CameraImageAnalysis class.
+
+        Args:
+            scan_directory (str or Path): Path to the scan directory containing data.
+            device_name (str): Name of the device to construct the subdirectory path.
+        """
+        super().__init__(scan_directory, device_name, use_gui=use_gui)
+
+    def create_cross_mask(self, image, cross_center, angle, cross_height=54,
+                          cross_width=54, thickness=8):
+        """
+        Creates a mask with a cross centered at `cross_center` with the cross being zeros and the rest ones.
+
+        Args:
+        - image (np.array): The image on which to base the mask size.
+        - cross_center (tuple): The (x, y) center coordinates of the cross.
+        - cross_height (int): The height of the cross extending vertically from the center.
+        - cross_width (int): The width of the cross extending horizontally from the center.
+        - thickness (int): The thickness of the lines of the cross.
+
+        Returns:
+        - np.array: The mask with the cross.
+        """
+        mask = np.ones_like(image, dtype=np.uint16)
+        x_center, y_center = cross_center
+        vertical_start, vertical_end = max(y_center - cross_height, 0), min(y_center + cross_height, image.shape[0])
+        horizontal_start, horizontal_end = max(x_center - cross_width, 0), min(x_center + cross_width, image.shape[1])
+        mask[vertical_start:vertical_end, x_center - thickness // 2:x_center + thickness // 2] = 0
+        mask[y_center - thickness // 2:y_center + thickness // 2, horizontal_start:horizontal_end] = 0
+
+        M = cv2.getRotationMatrix2D(cross_center, angle, 1.0)
+        rotated_mask = cv2.warpAffine(mask, M, (image.shape[1], image.shape[0]), flags=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT, borderValue=1)
+        return rotated_mask
+
+    def preprocess_visa_image(self, image, analysis_settings):
+        """
+        This function loads uses predefined analysis_settings to crop an image and mask out "crosses" from the images.
+
+        Args:
+        - image: loaded image to be processed.
+
+        """
+        mask1 = self.create_cross_mask(image, analysis_settings['Cross1'], analysis_settings['Rotate'])
+        mask2 = self.create_cross_mask(image, analysis_settings['Cross2'], analysis_settings['Rotate'])
+
+        masked_image = image * mask1 * mask2
+        cropped_image = masked_image[analysis_settings['Top ROI']:analysis_settings['Top ROI'] + analysis_settings['Size_Y'],
+                              analysis_settings['Left ROI']:analysis_settings['Left ROI'] + analysis_settings['Size_X']]
+
+        processed_image = cropped_image
+
+        return processed_image
+
+    def run_analysis(self):
+        """
+        Main function to UC_VisaEBeam image analysis.
+        """
+
+        # skip analysis if data is missing
+        if self.data_subdirectory is None or self.auxiliary_data is None:
+            logging.info(f"Skipping analysis due to missing data or auxiliary file.")
+            return
+
+        # preallocate storage
+        avg_images = []
+
+        # attempt analysis
+        try:
+
+            # identify unique parameter bins
+            unique_bins = np.unique(self.bins)
+            logging.info(f"unique_bins: {unique_bins}")
+
+            # iterate parameter bins
+            for bin_number in unique_bins:
+
+                # load all images for this bin
+                images = self.load_images_for_bin(bin_number)
+                if len(images) == 0:
+                    logging.warning(f"No images found for bin {bin_number}.")
+                    continue
+
+                # average the images
+                avg_image = self.average_images(images)
+
+                # check if camera analysis settings exist in config
+                if self.device_name in self.camera_analysis_configs.keys():
+
+                    # extract analysis settings, apply preprocessing routine
+                    analysis_settings = self.camera_analysis_configs[self.device_name]
+                    avg_image_processed = self.preprocess_visa_image(avg_image, analysis_settings)
+                    avg_images.append(avg_image_processed)
+
+                    # Save the averaged image
+                    save_name = f'{self.device_name}_{bin_number}_processed.png'
+                    self.save_image(avg_image_processed, save_dir=self.scan_directory, save_name=save_name)
+
+                else:
+                    avg_images.append(avg_image)
+
+                # save the averaged image
+                save_name = f'{self.device_name}_{bin_number}.png'
+                self.save_image(avg_image, save_dir=self.scan_directory, save_name=save_name)
+
+                logging.info(f"Averaged images for bin {bin_number} and saved as {save_name}.")
+
+            # Once all bins are processed, create an array of the averaged images
+            self.create_image_array(avg_images)
+
+        # throw warning if analysis fails
         except Exception as e:
             logging.warning(f"Warning: Image analysis failed due to: {e}")
             return
