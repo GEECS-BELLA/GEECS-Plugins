e07ca59f2a3946f91ee73408533dd9667cb5969b
Christopher Doss <CEDoss@lbl.gov>
2024-11-25 16:29:04 -0800

    Using dateparse for month_to_int function

--- GEECS-PythonAPI/geecs_python_api/controls/api_defs.py
+++ GEECS-PythonAPI/geecs_python_api/controls/api_defs.py
@@ -1,6 +1,7 @@
 # from __future__ import annotations
 from os import PathLike
 from pathlib import Path
+from dateutil.parser import parse as dateparse
 from threading import Thread, Event
 from typing import Optional, Any, Union, NamedTuple
 
@@ -32,20 +33,6 @@ class ScanTag(NamedTuple):
 
 def month_to_int(month: Union[str, int]) -> int:
     """ :return: an integer representing the given month """
-    month_map = {
-        'jan': 1, 'january': 1,
-        'feb': 2, 'february': 2,
-        'mar': 3, 'march': 3,
-        'apr': 4, 'april': 4,
-        'may': 5,
-        'jun': 6, 'june': 6,
-        'jul': 7, 'july': 7,
-        'aug': 8, 'august': 8,
-        'sep': 9, 'september': 9,
-        'oct': 10, 'october': 10,
-        'nov': 11, 'november': 11,
-        'dec': 12, 'december': 12,
-    }
     try:
         month_int = int(month)
         if 1 <= month_int <= 12:
@@ -53,8 +40,4 @@ def month_to_int(month: Union[str, int]) -> int:
     except ValueError:
         pass
 
-    month_lower = month.lower()
-    if month_lower in month_map:
-        return month_map[month_lower]
-
-    raise ValueError(f"Invalid month: {month}")
+    return dateparse(month).month
