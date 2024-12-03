4742af29f93f3ac4c170185bb19376c2eca8c25d
Christopher Doss <CEDoss@lbl.gov>
2024-12-03 15:03:01 -0800

    Some small changes/additions to improve clarity
    Brought the s_file regular expression to outside the classes, and the existing files now getting sorted before they are analyzed.  Also added documentation and type hinting

--- ScanAnalysis/live_watch/scan_watch.py
+++ ScanAnalysis/live_watch/scan_watch.py
@@ -5,13 +5,10 @@ ScanAnalysis.  If this remains the case, then TODO should make a shared version 
 from __future__ import annotations
 
 import time
-from argparse import ArgumentParser
-from datetime import datetime
 from pathlib import Path
 from time import sleep
 from typing import TYPE_CHECKING, Optional, Union
 import re
-from multiprocessing import Process
 from queue import Queue
 
 from geecs_python_api.analysis.scans.scan_data import ScanData
@@ -25,7 +22,6 @@ if TYPE_CHECKING:
     from watchdog.events import DirCreatedEvent, FileCreatedEvent
     from geecs_python_api.controls.api_defs import ScanTag
 
-import logging
 import logging.config
 import json
 import yaml
@@ -41,43 +37,62 @@ logging.config.dictConfig(
 logger = logging.getLogger("scan_analyzer")
 
 
-class AnalysisFolderEventHandler(FileSystemEventHandler):
-    s_filename_regex = re.compile(r"s(?P<scan_number>\d+).txt")  # TODO fix duplicate definition in ScanWatch
+def extract_s_file_number(filename: str) -> Optional[int]:
+    """ Tries to match a given file to a valid s-file, if applicable """
+    s_filename_regex = re.compile(r"s(?P<scan_number>\d+).txt")
+    m = s_filename_regex.match(filename)
+    if m is None:
+        return None
+    else:
+        return int(m['scan_number'])
 
-    def __init__(self, scan_watch_queue, experiment_name):
+
+class AnalysisFolderEventHandler(FileSystemEventHandler):
+    def __init__(self, scan_watch_queue: 'Queue', base_tag: 'ScanTag'):
         super().__init__()
         self.queue: Queue = scan_watch_queue
-        self.experiment = experiment_name
+        self.base_tag = base_tag
 
     def on_created(self, event: Union[DirCreatedEvent, FileCreatedEvent]):
+        """ If the event is a new file and a valid s-file, add the scan tag to the queue """
         # ignore new directories
         if event.is_directory:
             return
 
         date_folder_name, analysis_literal, filename = Path(event.src_path).parts[-3:]
         assert analysis_literal == 'analysis'
-        m = self.s_filename_regex.match(filename)
+        scan_number = extract_s_file_number(filename)
         # ignore anything other than sXX.txt files
-        if m is None:
+        if scan_number is None:
             return
 
         logger.info(f"Found new s-file: {filename}")
-        scan_number = int(m['scan_number'])
-        tag = ScanData.get_scan_tag(scan_watch.tag.year, scan_watch.tag.month, scan_watch.tag.day,
-                                    scan_number, experiment_name=self.experiment)
+        tag = ScanData.get_scan_tag(self.base_tag.year, self.base_tag.month, self.base_tag.day,
+                                    scan_number, experiment_name=self.base_tag.experiment)
         self.queue.put(tag)
 
 
 class ScanWatch:
-    def __init__(self, experiment_name: str, year, month, day,
-                 ignore_list=None, overwrite_previous=False):
+    def __init__(self, experiment_name: str, year: int, month: Union[int, str], day: int,
+                 ignore_list: list[int] = None, overwrite_previous: bool = False, perform_initial_search: bool = True):
         """
         Parameters
         ----------
-        date_folder_name : str
-            a HTU date folder name in the format 'yy_mmdd'
-        scan_analyzer : Optional[ScanAnalyzer]
-            use an existing ScanAnalyzer instance, or (default) create a new one.
+        experiment_name : str
+            A GEECS experiment name, IE 'Undulator'
+        year : int
+            Target directory year
+        month : Union[int, str]
+            Target directory month
+        day: int
+            Target directory day
+        ignore_list: list[int]
+            List of scan numbers to disregard for the given day
+        overwrite_previous: bool
+            Flag to ignore previously analyzed scan directories if True
+        perform_initial_search: bool
+            Flag to search through the target directory for existing scans to analyze
+
         """
         self.tag = ScanData.get_scan_tag(year, month, day, number=0, experiment_name=experiment_name)
         self.watch_folder = ScanData.build_scan_folder_path(tag=self.tag).parents[1] / "analysis"
@@ -92,10 +107,11 @@ class ScanWatch:
             self._read_processed_list()
 
         self.observer = PollingObserver()
-        self.observer.schedule(AnalysisFolderEventHandler(self.analysis_queue, experiment_name), str(self.watch_folder))
+        self.observer.schedule(AnalysisFolderEventHandler(self.analysis_queue, self.tag), str(self.watch_folder))
 
         # Initial check of scan folder
-        self.initial_search_of_watch_folder()
+        if perform_initial_search:
+            self.initial_search_of_watch_folder()
 
     def _check_watch_folder_exists(self, watch_folder_not_exist: str = 'raise'):
         """
@@ -152,10 +168,12 @@ class ScanWatch:
         logger.info(f"Started watching folder {self.watch_folder} for sXX.txt files.")
 
     def stop(self):
+        """ Stop the polling thread """
         self.observer.stop()
         self.observer.join()
 
     def process_queue(self):
+        """ Grab the next scan tag from the queue and attempt analysis """
         if self.analysis_queue.empty():
             return
 
@@ -165,27 +183,30 @@ class ScanWatch:
         self._evaluate_folder(tag, scan_folder)
 
     def initial_search_of_watch_folder(self):
+        """ Look for existing scans in the watch folder that have not been previously analyzed """
         logger.info(f"Searching for untouched scans on {self.tag.month}/{self.tag.day}/{self.tag.year}:")
-        s_filename_regex = re.compile(r"s(?P<scan_number>\d+).txt")
+        valid_scan_numbers = []
         if self.watch_folder.exists():
             for item in self.watch_folder.iterdir():
                 if item.is_file():
-                    m = s_filename_regex.match(str(item.name))
-                    if m is not None:
-                        scan_number = int(m['scan_number'])
-                        if scan_number not in self.processed_list:
-                            tag = ScanData.get_scan_tag(self.tag.year, self.tag.month,
-                                                        self.tag.day, scan_number, experiment_name=self.tag.experiment)
-                            self.analysis_queue.put(tag)
+                    scan_number = extract_s_file_number(str(item.name))
+                    if scan_number is not None and scan_number not in self.processed_list:
+                        valid_scan_numbers.append(scan_number)
+        sorted_scans = sorted(valid_scan_numbers)
+        for scan_number in sorted_scans:
+            tag = ScanData.get_scan_tag(self.tag.year, self.tag.month, self.tag.day, scan_number,
+                                        experiment_name=self.tag.experiment)
+            self.analysis_queue.put(tag)
         logger.info(f"Found {self.analysis_queue.qsize()} untouched scans.")
 
-    def _evaluate_folder(self, tag: ScanTag, scan_folder: Path):
+    def _evaluate_folder(self, tag: 'ScanTag', scan_folder: Path):
+        """ If there is a match for an analysis routine, perform the respective analysis(es) """
         logger.info(
             f"Starting analysis on scan {tag.month}/{tag.day}/{tag.year}:Scan{tag.number:03d}")
         valid_analyzers = check_for_analysis_match(scan_folder)
 
         try:
-            analyze_scan(tag, valid_analyzers)
+            analyze_scan(tag, valid_analyzers, debug_mode=True)
         except Exception as err:
             logger.error(f"Error in analyze_scan {tag.month}/{tag.day}/{tag.year}:Scan{tag.number:03d}): {err}")
 
@@ -194,6 +215,7 @@ class ScanWatch:
         logger.info(f"Finished analysis on scan {tag.month}/{tag.day}/{tag.year}:Scan{tag.number:03d}")
 
     def _read_processed_list(self):
+        """ Add all scans numbers found in the yaml to the processed list """
         contents = self._read_yaml_file()
 
         year_data = contents.get(str(self.tag.year)) if contents is not None else None
@@ -205,6 +227,7 @@ class ScanWatch:
                 self.processed_list.append(scan)
 
     def _read_yaml_file(self) -> dict:
+        """ Read yaml file for the dictionary """
         contents = None
         if self.processed_list_filename.exists():
             with open(self.processed_list_filename, 'r') as file:
@@ -212,6 +235,7 @@ class ScanWatch:
         return contents
 
     def _write_processed_list(self):
+        """ Update yaml file with the new processed list """
         data = self._read_yaml_file()
         new_contents = {str(self.tag.year): {str(self.tag.month): {str(self.tag.day): self.processed_list}}}
         if data is None:
@@ -224,6 +248,7 @@ class ScanWatch:
 
 
 def recursive_update(base: dict, new: dict) -> dict:
+    """ Recursively update a dictionary with new information """
     for key, value in new.items():
         if isinstance(value, dict):
             base[key] = recursive_update(base.get(key, {}), value)
@@ -246,11 +271,11 @@ if __name__ == '__main__':
                     )"""
 
     exp = 'Undulator'
-    year = 2024
-    month = 11
-    day = 5
+    test_year = 2024
+    test_month = 11
+    test_day = 5
 
-    scan_watch = ScanWatch(experiment_name=exp, year=year, month=month, day=day)
+    scan_watch = ScanWatch(experiment_name=exp, year=test_year, month=test_month, day=test_day)
     print("Starting...")
     scan_watch.start(watch_folder_not_exist='wait')
 
