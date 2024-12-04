c5e1098afafb48c56682e5efe8a984af73de62ec
Christopher Doss <CEDoss@lbl.gov>
2024-12-04 12:48:58 -0800

    Moved __main__ methods to unit tests

--- ScanAnalysis/scan_analysis/execute_scan_analysis.py
+++ ScanAnalysis/scan_analysis/execute_scan_analysis.py
@@ -20,25 +20,3 @@ def analyze_scan(tag: 'ScanTag', analyzer_list: list['AnalyzerInfo'], debug_mode
             if index_of_files:  # TODO And if a Google doc procedure is defined for the given experiment
                 # TODO Append the images to the appropriate location in the daily experiment log on Google
                 pass
-
-
-if __name__ == '__main__':  # TODO move to a unit test
-    from geecs_python_api.controls.api_defs import ScanTag
-    from scan_analysis.mapping.map_Undulator import undulator_analyzers
-
-    # Given scan tag and string for analysis:
-    test_tag = ScanTag(year=2024, month=11, day=5, number=5, experiment='Undulator')
-    test_analyzer = undulator_analyzers[0]  # MagSpec
-
-    # Convert string to analysis class and call analysis with scan tag
-    analyze_scan(test_tag, [test_analyzer])
-    print("Done with MagSpec")
-
-    ######################
-
-    test_tag = ScanTag(year=2024, month=11, day=26, number=19, experiment='Undulator')
-    test_analyzer = undulator_analyzers[1]  # Visa Undulator
-
-    # Convert string to analysis class and call analysis with scan tag
-    analyze_scan(test_tag, [test_analyzer])
-    print("Done with Visa")
--- ScanAnalysis/scan_analysis/mapping/scan_evaluator.py
+++ ScanAnalysis/scan_analysis/mapping/scan_evaluator.py
@@ -67,31 +67,3 @@ def get_available_directories(root: Path) -> list[str]:
     except FileNotFoundError:
         print(f"Folder '{root}' does not exist.")
         return []
-
-
-if __name__ == '__main__':
-    exp = 'Undulator'
-
-    print("Scan 21 should just be the VISA, Rad2")
-    folder = "Z:\\data\\Undulator\\Y2024\\11-Nov\\24_1105\\scans\\Scan021"
-    results = check_for_analysis_match(folder, exp)
-    for a in results:
-        print(a.analyzer_class.__name__, a.device_name)
-
-    print("Scan 7 should just be the Mag Spec")
-    folder = "Z:\\data\\Undulator\\Y2024\\11-Nov\\24_1105\\scans\\Scan007"
-    results = check_for_analysis_match(folder, exp)
-    for a in results:
-        print(a.analyzer_class.__name__, a.device_name)
-
-    print("Scan 12 should just be the ALine3")
-    folder = "Z:\\data\\Undulator\\Y2024\\11-Nov\\24_1105\\scans\\Scan012"
-    results = check_for_analysis_match(folder, exp)
-    for a in results:
-        print(a.analyzer_class.__name__, a.device_name)
-
-    print("Previously using Master Control, nearly all would be flagged every time")
-    folder = "Z:\\data\\Undulator\\Y2024\\06-Jun\\24_0606\\scans\\Scan003"
-    results = check_for_analysis_match(folder, exp)
-    for a in results:
-        print(a.analyzer_class.__name__, a.device_name)
