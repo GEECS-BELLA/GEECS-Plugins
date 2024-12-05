73d9d0b2186d00585313a47ad62aef9da70262f1
ajgonsalves <tony_g@outlook.com>
2024-12-05 12:47:28 -0800

    PW configuration for badger: control variables added

--- Xopt-GEECS/badger-plugins/environments/pw/Bella_PW_General/__init__.py
+++ Xopt-GEECS/badger-plugins/environments/pw/Bella_PW_General/__init__.py
@@ -7,7 +7,23 @@ class Environment(geecs_Environment):
     name = 'Bella_PW_General'
     variables = {
         'HEX-PL1-1:ypos': [-8,2],
-        'HEX-PL1-1:wangle': [-2,2]
+        'HEX-PL1-1:wangle': [-2,2],
+        'HEX-PL1-1:zpos': [-5,0],
+        'HEX-PL1-1:vangle': [0,1],
+        'stage-2bl-compressor:position.axis 1': [-4.5,6],
+        'stage-hpd-tel:position': [180,600],
+        'stage-1bl-compression:position.axis 1': [-5,6],
+        'mcd-1bl-esp302m4m5:position.axis 1': [-1.2,-1.15],
+        'mcd-1bl-esp302m4m5:position.axis 2': [-0.93,-0.89],
+        'mcd-1bl-esp302m4m5:position.axis 3': [-0.8,0.84],
+        'prc-pl1-highpressure:pressure.device 1': [50,150],
+        'prc-pl1-highpressure:pressure.device 2': [50,150],
+        'mcd-1bl-esp302m2m4:position.axis 1': [0.98,1.02],
+        'mcd-1bl-esp302m2m4:position.axis 2': [-1.4,-1.18],
+        'mcd-1bl-esp302m2m4:position.axis 3': [-0.78,-0.74],
+        'hex-pl1-2:ypos': [-0.8,1],
+        'hex-pl1-2:zpos': [-1.6,-1.4],
+
     }
     observables = ['CAM-PL1-1-SideView:meancounts',
                    'CAM-HPD-CCD:maxcounts']
