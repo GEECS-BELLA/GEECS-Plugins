f4b0ead25bf8c0ed98ce50ea713546e288ee1ad2
skbarber <64565579+skbarber@users.noreply.github.com>
2024-12-09 23:29:54 -0800

    dummy radspec2analysis now runs camera analysis by default rather than nothing. Proper analysis routine to be defined

--- ScanAnalysis/scan_analysis/analyzers/Undulator/Rad2SpecAnalysis.py
+++ ScanAnalysis/scan_analysis/analyzers/Undulator/Rad2SpecAnalysis.py
@@ -11,8 +11,8 @@ from geecs_python_api.controls.api_defs import ScanTag
 
 
 class Rad2SpecAnalysis(CameraImageAnalysis):
-    def __init__(self, scan_tag: 'ScanTag', skip_plt_show: bool = True):
+    def __init__(self, scan_tag: 'ScanTag', device_name: Optional[str] = None, skip_plt_show: bool = True):
         super().__init__(scan_tag=scan_tag, device_name='UC_UndulatorRad2', skip_plt_show=skip_plt_show)
 
-    def run_analysis(self, config_options: Optional[str] = None):
-        pass
+    # def run_analysis(self, config_options: Optional[str] = None):
+        # pass
