2b2f61e4b16b5f1187f2c99220e930a7f9ecae1b
Christopher Doss <CEDoss@lbl.gov>
2024-12-11 16:29:38 -0800

    Updating analyzer mapping for HTU
    Removed ICT for basic mag spec stitcher, and added the other diagnostics screens for basic camera image analysis

--- ScanAnalysis/scan_analysis/mapping/map_Undulator.py
+++ ScanAnalysis/scan_analysis/mapping/map_Undulator.py
@@ -8,7 +8,7 @@ from scan_analysis.analyzers.Undulator.HiResMagCamAnalysis import HiResMagCamAna
 
 undulator_analyzers = [
     Info(analyzer_class=MagSpecStitcherAnalysis,
-         requirements={'AND': ['U_BCaveICT', 'U_BCaveMagSpec']},
+         requirements={'U_BCaveMagSpec'},
          device_name='U_BCaveMagSpec'),
     Info(analyzer_class=VisaEBeamAnalysis,
          requirements={'OR': ['UC_VisaEBeam1', 'UC_VisaEBeam2', 'UC_VisaEBeam3', 'UC_VisaEBeam4',
@@ -18,8 +18,23 @@ undulator_analyzers = [
                                {'OR': ['UC_VisaEBeam1', 'UC_VisaEBeam2', 'UC_VisaEBeam3', 'UC_VisaEBeam4',
                                        'UC_VisaEBeam5', 'UC_VisaEBeam6', 'UC_VisaEBeam7', 'UC_VisaEBeam8']}]}),
     Info(analyzer_class=CameraImageAnalysis,
+         requirements={'UC_ALineEBeam1'},
+         device_name='UC_ALineEBeam1'),
+    Info(analyzer_class=CameraImageAnalysis,
+         requirements={'UC_ALineEBeam2'},
+         device_name='UC_ALineEBeam2'),
+    Info(analyzer_class=CameraImageAnalysis,
          requirements={'UC_ALineEBeam3'},
          device_name='UC_ALineEBeam3'),
+    Info(analyzer_class=CameraImageAnalysis,
+         requirements={'UC_TC_Phosphor'},
+         device_name='UC_TC_Phosphor'),
+    Info(analyzer_class=CameraImageAnalysis,
+         requirements={'UC_DiagnosticsPhosphor'},
+         device_name='UC_DiagnosticsPhosphor'),
+    Info(analyzer_class=CameraImageAnalysis,
+         requirements={'UC_Phosphor1'},
+         device_name='UC_Phosphor1'),
     Info(analyzer_class=HiResMagCamAnalysis,
-         requirements={'AND': ['UC_HiResMagCam', 'U_BCaveICT']})
+         requirements={'AND': ['UC_HiResMagCam', 'U_BCaveICT']}),
 ]
