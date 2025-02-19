e2aea446911a589a77bfce6ebcce92d0dafbd025
Christopher Doss <CEDoss@lbl.gov>
2025-02-18 17:07:40 -0800

    Function to get the url for latest scanlog
    Also cleaned up the generator function for latest scan number.

--- GEECS-Scanner-GUI/geecs_scanner/utils/module_open_folder.py
+++ GEECS-Scanner-GUI/geecs_scanner/utils/module_open_folder.py
@@ -1,8 +1,10 @@
 """
 Functions for opening file explorer on the given folders.  TODO make a Mac-compatible version if needed (or throw error)
 """
+from typing import Optional
 import re
 import subprocess
+import configparser
 from pathlib import Path
 from geecs_python_api.analysis.scans.scan_data import ScanData
 
@@ -19,19 +21,58 @@ def open_daily_data_folder(experiment: str):
     open_folder(path_to_folder=latest)
 
 
+def iterate_scan_numbers(scan_folder: Path):
+    pattern = re.compile(r'^Scan(\d+)$')
+    if not scan_folder.exists():
+        return
+    for p in scan_folder.iterdir():
+        if p.is_dir() and (m := pattern.match(p.name)):
+            yield int(m.group(1))
+
+
 def get_latest_scan_number(experiment: str) -> int:
     """ Finds the latest scan number using regular expressions rather than iterating through ScanData scan tags
 
     :return: int for latest scan number, 0 if no scans that day
     """
     scan_folder = ScanData.get_daily_scan_folder(experiment=experiment)
-    pattern = re.compile(r'^Scan(\d+)$')
+    return max(iterate_scan_numbers(scan_folder), default=0)
 
-    max_scan_folder: Path = max(filter(lambda p: pattern.match(p.name), scan_folder.iterdir()), default=None)
-    if max_scan_folder:
-        return int(pattern.match(max_scan_folder.name).group(1))
-    else:
-        return 0
+
+def get_experiment_scanlog_url(experiment: str) -> Optional[str]:
+    """
+    Maps an expeirment name to a routine used to read the current day's experiment log
+
+    :param experiment: The experiment name
+    :return: a string pointing to the
+    """
+    experiment_to_func = {
+        'Undulator': get_scanlog_undulator,
+    }
+    if func := experiment_to_func.get(experiment, None):
+        if url := func():
+            return url
+
+
+def get_scanlog_undulator() -> Optional[str]:
+    base_folder = Path("Z:\software\control-all-loasis\HTU\Active Version\GEECS-Plugins\LogMaker4GoogleDocs\logmaker_4_googledocs")
+    if not base_folder.exists():
+        return
+
+    config_file = 'HTUparameters.ini'
+    config_path = base_folder / config_file
+
+    # Load the configuration file
+    experiment_config = configparser.ConfigParser()
+    experiment_config.read(config_path)
+
+    # Debugging: Verify the loaded sections
+    if not experiment_config.sections():
+        return
+
+    document_id = experiment_config['DEFAULT']['logid']
+    url = f"https://docs.google.com/document/d/{document_id}"
+    return url
 
 
 def reload_scan_data_paths():
