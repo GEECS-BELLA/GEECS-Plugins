0c580a34ae3fe63a737e38131a6f423b7fdd2077
Christopher Doss <CEDoss@lbl.gov>
2025-02-26 15:12:00 -0800

    Optional argument for number of lines to show

--- GEECS-Scanner-GUI/geecs_scanner/app/lib/gui_utilities.py
+++ GEECS-Scanner-GUI/geecs_scanner/app/lib/gui_utilities.py
@@ -10,16 +10,18 @@ from PyQt5.QtWidgets import QLineEdit, QCompleter
 from PyQt5.QtCore import Qt, QObject
 
 
-def display_completer_list(window: QObject, location: QLineEdit, completer_list: list[str]):
+def display_completer_list(window: QObject, location: QLineEdit, completer_list: list[str], max_visible_lines: int = 6):
     """ Displays a completer list at a given location
 
     :param window: GUI window that calls this (ie; use 'self')
     :param location: GUI element at which to show the completer list
     :param completer_list: strings to show in the completer pop-up
+    :param max_visible_lines: maximum number of completer entries to show
     """
     if location.isEnabled():
         location.selectAll()
         completer = QCompleter(completer_list, window)
+        completer.setMaxVisibleItems(max_visible_lines)
         completer.setCompletionMode(QCompleter.PopupCompletion)
         completer.setCaseSensitivity(Qt.CaseSensitive)
 
