3a822e60ff04560a2acf447f9feb90830552a605
skbarber <64565579+skbarber@users.noreply.github.com>
2025-03-12 13:37:48 -0700

    Update test_probe_phase_analysis.py

--- tests/ScannerTools/test_scanner_tools/test_probe_phase_analysis.py
+++ tests/ScannerTools/test_scanner_tools/test_probe_phase_analysis.py
@@ -13,15 +13,15 @@ from geecs_python_api.analysis.scans.scan_data import ScanData, ScanTag
 from image_analysis.analyzers.density_from_phase_analysis import PhaseAnalysisConfig, PhasePreprocessor, PhaseDownrampProcessor
 
 def get_path_to_phase_file():
-    st = ScanTag(2025, 2, 19, 3, experiment='Undulator')
+    st = ScanTag(2025, 3, 6, 16, experiment='Undulator')
     s_data = ScanData(tag=st)
-    path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'Scan003_U_HasoLift_002_postprocessed.tsv')
+    path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'Scan016_U_HasoLift_002_postprocessed.tsv')
     return path_to_file
 
 def get_path_to_bkg_file():
-    st = ScanTag(2025, 2, 19, 2, experiment='Undulator')
+    st = ScanTag(2025, 3, 6, 15, experiment='Undulator')
     s_data = ScanData(tag=st)
-    path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'average_phase.tsv')
+    path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'average_phase2.tsv')
     return path_to_file
 
 def test_get_path_to_haso_file():
@@ -35,8 +35,8 @@ def test_phase_processing():
     config: PhaseAnalysisConfig = PhaseAnalysisConfig(
         pixel_scale=10.1,            # um per pixel (vertical)
         wavelength_nm=800,           # Probe laser wavelength in nm
-        threshold_fraction=0.2,      # Threshold fraction for pre-processing
-        roi=(10, -10, 1, -100),      # Example ROI: (x_min, x_max, y_min, y_max)
+        threshold_fraction=0.3,      # Threshold fraction for pre-processing
+        roi=(10, -10, 50, -200),      # Example ROI: (x_min, x_max, y_min, y_max)
         background=bkg_file_path  # Background is now a Path
     )
 
