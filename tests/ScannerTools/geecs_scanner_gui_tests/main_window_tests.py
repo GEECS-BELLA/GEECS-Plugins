ba0e232951004c58fb6ed5e525acbd3f29ff0c5e
Christopher Doss <CEDoss@lbl.gov>
2025-03-14 11:35:29 -0700

    Externally sets application paths and exp name

--- tests/ScannerTools/geecs_scanner_gui_tests/main_window_tests.py
+++ tests/ScannerTools/geecs_scanner_gui_tests/main_window_tests.py
@@ -1,17 +1,28 @@
-import sys
 import pytest
+
+from pathlib import Path
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 
 from geecs_scanner.app.geecs_scanner import GEECSScannerWindow
+from geecs_scanner.utils import ApplicationPaths as AppPaths
 
 
 @pytest.fixture
 def app(qtbot):
-    window = GEECSScannerWindow()
+    window = GEECSScannerWindow(unit_test_mode=True)
     qtbot.addWidget(window)
     window.show()
+
+    # Hard-code application paths to test experiment in this package
+    AppPaths.BASE_PATH = Path(__file__).parent / "test_configs"
+    window.app_paths = AppPaths(experiment="Test", create_new=False)
+
+    # Set experiment name to "Test" and update the GUI accordingly
+    window.ui.experimentDisplay.setText("Test")
+
     return window
 
 
