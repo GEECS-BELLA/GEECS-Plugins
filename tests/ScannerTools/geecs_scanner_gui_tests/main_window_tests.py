9f7556e85f80cb3d853ce861b5929d01d2716926
Christopher Doss <CEDoss@lbl.gov>
2025-03-14 15:18:57 -0700

    Tests moving elements between the two lists

--- tests/ScannerTools/geecs_scanner_gui_tests/main_window_tests.py
+++ tests/ScannerTools/geecs_scanner_gui_tests/main_window_tests.py
@@ -1,9 +1,8 @@
 import pytest
+from pytestqt.qtbot import QtBot
 
 from pathlib import Path
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 
 from geecs_scanner.app.geecs_scanner import GEECSScannerWindow
@@ -11,10 +10,12 @@ from geecs_scanner.utils import ApplicationPaths as AppPaths
 
 
 @pytest.fixture
-def app(qtbot):
+def app(qtbot: QtBot):
     window = GEECSScannerWindow(unit_test_mode=True)
     qtbot.addWidget(window)
     window.show()
+    window.activateWindow()
+    window.setFocus()
 
     # Hard-code application paths to test experiment in this package
     AppPaths.BASE_PATH = Path(__file__).parent / "test_configs"
@@ -24,6 +25,29 @@ def app(qtbot):
     window.ui.experimentDisplay.setText("Test")
 
     return window
+
+
+def test_element_list(app, qtbot: QtBot):
+    list1 = app.ui.foundDevices
+    list2 = app.ui.selectedDevices
+    assert list1.count() == 2
+
+    list1.setCurrentRow(0)
+    qtbot.mouseClick(app.ui.addDeviceButton, Qt.LeftButton)
+    assert list1.count() == 1
+    assert list2.count() == 1
+
+    list2.setCurrentRow(0)
+    qtbot.mouseClick(app.ui.removeDeviceButton, Qt.LeftButton)
+    assert list1.count() == 2
+    assert list2.count() == 0
+
+    list1.setCurrentRow(1)
+    qtbot.mouseClick(app.ui.addDeviceButton, Qt.LeftButton)
+    list1.setCurrentRow(0)
+    qtbot.mouseClick(app.ui.addDeviceButton, Qt.LeftButton)
+    assert list1.count() == 0
+    assert list2.count() == 2
 
 
 def test_button_click(app, qtbot):
