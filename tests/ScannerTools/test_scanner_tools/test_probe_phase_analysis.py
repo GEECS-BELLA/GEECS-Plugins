7da0c02c4996e72c65b539fc0de3c01bd8bf879f
skbarber <64565579+skbarber@users.noreply.github.com>
2025-03-15 01:56:43 -0700

    tuned the test analyzer for density downramp phase for undulator

--- tests/ScannerTools/test_scanner_tools/test_probe_phase_analysis.py
+++ tests/ScannerTools/test_scanner_tools/test_probe_phase_analysis.py
@@ -14,14 +14,16 @@ from image_analysis.offline_analyzers.density_from_phase_analysis import PhaseAn
 def get_path_to_phase_file():
     st = ScanTag(2025, 3, 6, 16, experiment='Undulator')
     s_data = ScanData(tag=st)
-    path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'Scan016_U_HasoLift_002_postprocessed.tsv')
+    path_to_file = Path(s_data.get_folder() / "U_HasoLift" / 'Scan016_U_HasoLift_003_postprocessed.tsv')
+    # path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'Scan016_U_HasoLift_002_postprocessed.tsv')
     print(path_to_file)
     return path_to_file
 
 def get_path_to_bkg_file():
     st = ScanTag(2025, 3, 6, 15, experiment='Undulator')
     s_data = ScanData(tag=st)
-    path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'average_phase2.tsv')
+    # path_to_file = Path(s_data.get_analysis_folder() / "U_HasoLift" / "HasoAnalysis" / 'average_phase2.tsv')
+    path_to_file = Path(s_data.get_folder() / "U_HasoLift" / 'average_phase.tsv')
     return path_to_file
 
 def test_get_path_to_haso_file():
@@ -37,12 +39,13 @@ def test_phase_processing():
     config: PhaseAnalysisConfig = PhaseAnalysisConfig(
         pixel_scale=10.1,            # um per pixel (vertical)
         wavelength_nm=800,           # Probe laser wavelength in nm
-        threshold_fraction=0.3,      # Threshold fraction for pre-processing
-        roi=(10, -10, 50, -200),      # Example ROI: (x_min, x_max, y_min, y_max)
+        threshold_fraction=0.1,      # Threshold fraction for pre-processing
+        roi=(10, -10, 75, -250),      # Example ROI: (x_min, x_max, y_min, y_max)
         background=bkg_file_path  # Background is now a Path
     )
 
     processor  = PhaseDownrampProcessor(config)
+    processor.use_interactive = True
     processor.analyze_image( file_path = phase_file_path)
 
 
