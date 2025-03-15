aa0708607a0772151a445b6fc1b5bcc5347262aa
skbarber <64565579+skbarber@users.noreply.github.com>
2025-03-14 20:27:33 -0700

    update analysis mapping fro undulator

--- ScanAnalysis/scan_analysis/mapping/map_Undulator.py
+++ ScanAnalysis/scan_analysis/mapping/map_Undulator.py
@@ -1,3 +1,6 @@
+from geecs_python_api.controls.api_defs import ScanTag
+from geecs_python_api.analysis.scans.scan_data import ScanData
+
 from scan_analysis.base import AnalyzerInfo as Info
 
 from scan_analysis.analyzers.Undulator.hi_res_mag_cam_analysis import HiResMagCamAnalysis
@@ -5,15 +8,35 @@ from scan_analysis.analyzers.Undulator.mag_spec_stitcher_analysis import MagSpec
 from scan_analysis.analyzers.Undulator.rad2_spec_analysis import Rad2SpecAnalysis
 from scan_analysis.analyzers.Undulator.visa_ebeam_analysis import VisaEBeamAnalysis
 from scan_analysis.analyzers.Undulator.camera_image_analysis import CameraImageAnalysis
-from scan_analysis.analyzers.Undulator.array2D_scan_analysis import Array2DScanAnalysis
 from scan_analysis.analyzers.Undulator.hamaspectro_analysis import FiberSpectrometerAnalysis
 from scan_analysis.analyzers.Undulator.frog_analysis import FrogAnalysis
 
-from image_analysis.offline_analyzers.basic_image_analysis import BasicImageAnalyzer
+from scan_analysis.analyzers.Undulator.array2D_scan_analysis import Array2DScanAnalysis
+from scan_analysis.analyzers.Undulator.HIMG_with_average_saving import HIMGWithAveraging
+
+
+
 from image_analysis.offline_analyzers.Undulator.ACaveMagCam3 import ACaveMagCam3ImageAnalyzer
 from image_analysis.offline_analyzers.HASO_himg_has_processor import HASOHimgHasProcessor
+from image_analysis.offline_analyzers.density_from_phase_analysis import PhaseAnalysisConfig, PhaseDownrampProcessor
 
 
+def get_path_to_bkg_file():
+    st = ScanTag(2025, 2, 19, 2, experiment='Undulator')
+    s_data = ScanData(tag=st)
+    path_to_file = s_data.get_folder() / 'U_HasoLift' / 'average_phase.tsv'
+
+    return path_to_file
+
+
+bkg_file_path = get_path_to_bkg_file()
+phase_analysis_config: PhaseAnalysisConfig = PhaseAnalysisConfig(
+    pixel_scale=10.1,  # um per pixel (vertical)
+    wavelength_nm=800,  # Probe laser wavelength in nm
+    threshold_fraction=0.3,  # Threshold fraction for pre-processing
+    roi=(10, -10, 50, -200),  # Example ROI: (x_min, x_max, y_min, y_max)
+    background=bkg_file_path  # Background is now a Path
+)
 
 undulator_analyzers = [
     Info(analyzer_class=MagSpecStitcherAnalysis,
@@ -57,8 +80,15 @@ undulator_analyzers = [
          requirements={'UC_ACaveMagCam3'},
          device_name='UC_ACaveMagCam3',
          image_analyzer_class = ACaveMagCam3ImageAnalyzer),
+    Info(analyzer_class=HIMGWithAveraging,
+         requirements={'U_HasoLift'},
+         device_name='U_HasoLift',
+         image_analyzer_class=HASOHimgHasProcessor,
+         file_pattern = "*_{shot_num:03d}.himg"),
     Info(analyzer_class=Array2DScanAnalysis,
          requirements={'U_HasoLift'},
          device_name='U_HasoLift',
-         image_analyzer_class=HASOHimgHasProcessor)
+         image_analyzer_class=PhaseDownrampProcessor,
+         file_pattern = "*_{shot_num:03d}_postprocessed.himg",
+         image_analysis_config = phase_analysis_config )
 ]
