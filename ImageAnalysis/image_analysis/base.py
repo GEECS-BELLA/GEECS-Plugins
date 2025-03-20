950df7b36609bcfabeeaac15457d49e0a84a2820
skbarber <64565579+skbarber@users.noreply.github.com>
2025-03-20 14:42:15 -0700

    improved implementation of analyzing image from file path in the base ImageAnalyzer
    new method analyze_image_file can be used in place of analyze_image, which loads an image and sends it to analyze_image. For non standard file types, the load_image method should be overwritten in teh device's image analysis

--- ImageAnalysis/image_analysis/base.py
+++ ImageAnalysis/image_analysis/base.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 import configparser
 import numpy as np
 from numpy.typing import NDArray
+from pathlib import Path
 from typing import TYPE_CHECKING, Optional, Union, Type, Any
 if TYPE_CHECKING:
     from .types import Array2D
+
+from utils import read_imaq_image
 
 
 class ImageAnalyzer:
@@ -173,6 +176,50 @@ class ImageAnalyzer:
             "analyzer_return_lineouts": return_lineouts,
         }
         return return_dictionary
+
+    def analyze_image_file(self,
+                           image_filepath: Path,
+                           auxiliary_data: Optional[dict] = None
+                           )-> dict[str, Union[float, int, str, np.ndarray]]:
+        """
+        Method to enable the use of a file path rather than Array2D.
+
+         Parameters
+         ----------
+         image_filepath : Path
+
+        Returns
+        -------
+        analysis : dict[str, Union[float, np.ndarray]]
+            metric name as key. value can be a float, int, str, 1d array, 2d array, etc.
+
+        """
+
+        image = self.load_image(image_filepath)
+
+        return self.analyze_image(image, auxiliary_data)
+
+    @staticmethod
+    def load_image(file_path:Path)->Array2D:
+        """
+        load an image from a path. By default, the read_imaq_png function is used.
+        For file types not directly supported by this method, e.g. .himg files from a
+        Haso device type, this method be implemented in that device's ImageAnalyzer
+        subclass.
+
+         Parameters
+         ----------
+         file_path : Path
+
+         Returns
+         -------
+         image : Array2D
+        """
+
+        image = Array2D(read_imaq_image(file_path))
+
+        return image
+
 
     def build_input_parameter_dictionary(self) -> dict:
         """Compiles list of class variables into a dictionary
