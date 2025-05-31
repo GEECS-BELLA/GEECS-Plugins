bb23a71044b9541cbc01205edfbf03c631c6a5d8
skbarber <64565579+skbarber@users.noreply.github.com>
2025-05-31 09:22:53 -0700

    add scanmode enum type to distinguish between noscan,standard scan, and optimization
    cleaner way to handle scan types rather than just using strings

--- GEECS-Scanner-GUI/geecs_scanner/data_acquisition/types.py
+++ GEECS-Scanner-GUI/geecs_scanner/data_acquisition/types.py
@@ -1,12 +1,19 @@
 from dataclasses import dataclass
 from typing import Optional, Union
+from enum import Enum, auto
+
+class ScanMode(Enum):
+    STANDARD = auto()
+    NOSCAN = auto()
+    OPTIMIZATION = auto()
 
 @dataclass
 class ScanConfig:
-    device_var: str = 'noscan'
+    scan_mode: ScanMode = ScanMode.NOSCAN
+    device_var: Optional[str] = None
     start: Union[int, float] = 0
     end: Union[int, float] = 1
     step: Union[int, float] = 1
     wait_time: float = 1.0
-    additional_description: str = ''
+    additional_description: Optional[str] = None
     background: bool = False
