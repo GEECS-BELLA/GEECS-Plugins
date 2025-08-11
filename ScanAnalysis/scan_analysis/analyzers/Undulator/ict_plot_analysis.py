14cb1ef6bf78ce73ab572cc7f6ce3ffe161afb9f
Christopher Doss <cedoss@lbl.gov>
2025-08-11 14:39:03 -0700

    feat: can specify whether to use median or mean

--- ScanAnalysis/scan_analysis/analyzers/Undulator/ict_plot_analysis.py
+++ ScanAnalysis/scan_analysis/analyzers/Undulator/ict_plot_analysis.py
@@ -7,11 +7,11 @@ from scan_analysis.analyzers.common.scatter_plotter_analysis import ScatterPlott
 
 
 class ICTPlotAnalysis(ScatterPlotterAnalysis):
-    def __init__(self, scan_tag: ScanTag, skip_plt_show: bool = True, flag_logging: bool = False):
+    def __init__(self, scan_tag: ScanTag, use_median: bool = True, skip_plt_show: bool = True, flag_logging: bool = False):
         super().__init__(scan_tag=scan_tag,
                          device_name='U_BCaveICT',
                          scatter_plot=False,
-                         use_median=True,
+                         use_median=use_median,
                          data_key_1='U_BCaveICT Python Results.ChA Alias:U_BCaveICT Charge pC',
                          label_1='BCave ICT Charge',
                          ylabel_1='Charge (pC)',
@@ -23,5 +23,5 @@ if __name__ == "__main__":
     from geecs_data_utils import ScanData
 
     tag = ScanData.get_scan_tag(year=2025, month=7, day=3, number=1, experiment='Undulator')
-    analyzer = ICTPlotAnalysis(scan_tag=tag, skip_plt_show=False, flag_logging=True)
+    analyzer = ICTPlotAnalysis(scan_tag=tag, skip_plt_show=False, flag_logging=True, use_median=True)
     analyzer.run_analysis()
