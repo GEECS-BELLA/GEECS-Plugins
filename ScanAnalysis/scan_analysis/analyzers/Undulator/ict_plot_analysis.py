d73dadfdecd0612dd26a15deb97c2a08ed13a9b6
Christopher Doss <cedoss@lbl.gov>
2025-08-11 13:04:19 -0700

    feat: bcave ict charge vs scan parameter plotter

---
+++ ScanAnalysis/scan_analysis/analyzers/Undulator/ict_plot_analysis.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from geecs_data_utils import ScanTag
+
+from scan_analysis.analyzers.common.scatter_plotter_analysis import ScatterPlotterAnalysis
+
+
+class ICTPlotAnalysis(ScatterPlotterAnalysis):
+    def __init__(self, scan_tag: ScanTag, skip_plt_show: bool = True, flag_logging: bool = False):
+        super().__init__(scan_tag=scan_tag,
+                         device_name='U_BCaveICT',
+                         scatter_plot=False,
+                         use_median=True,
+                         data_key_1='U_BCaveICT Python Results.ChA Alias:U_BCaveICT Charge pC',
+                         label_1='BCave ICT Charge',
+                         ylabel_1='Charge (pC)',
+                         skip_plt_show=skip_plt_show,
+                         flag_logging=flag_logging)
+
+
+if __name__ == "__main__":
+    from geecs_data_utils import ScanData
+
+    tag = ScanData.get_scan_tag(year=2025, month=7, day=3, number=1, experiment='Undulator')
+    analyzer = ICTPlotAnalysis(scan_tag=tag, skip_plt_show=False, flag_logging=True)
+    analyzer.run_analysis()
