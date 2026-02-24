42fe8f354bd555bf5ef8ac666e8db75f4972d962
skbarber <64565579+skbarber@users.noreply.github.com>
2026-02-24 09:08:47 -0500

    formatting

--- ScanAnalysis/scan_analysis/analyzers/common/array2D_scan_analysis.py
+++ ScanAnalysis/scan_analysis/analyzers/common/array2D_scan_analysis.py
@@ -234,7 +234,7 @@ class Array2DScanAnalyzer(SingleDeviceScanAnalyzer):
                 for shot_num, result in self.results.items()
             ]
             # TODO: saving a gif should optional and configurable
-            save_gif=False
+            save_gif = False
             if save_gif:
                 animation_path = self.path_dict["save"] / "noscan.gif"
                 self.renderer.render_animation(contexts, config, animation_path)
